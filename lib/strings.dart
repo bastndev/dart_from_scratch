4208df44100df541b95f7cece962137723607b83
bastndev <gohit24@gmail.com>
2023-12-07 16:57:36 -0500

    :alien: Update `Learning`

--- lib/strings.dart
+++ lib/strings.dart
@@ -1,7 +1,19 @@
-void main() {
+// --- --- -TODO: Strings length and contains
+/* void main() {
   String name = "Bastian";
 
   int you = name.length;
 
   print("Your name have $you characters");
+}
+ */
+
+void main() {
+  String name = "Bastian";
+
+  bool me = name.contains("Ba");
+  bool me2 = name.contains("ba");
+
+  print("You name contains Ba?: $me");
+  print("You name contains Ba?: $me2");
 }
