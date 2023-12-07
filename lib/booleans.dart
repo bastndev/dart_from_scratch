0f44b887f288befb547cdec5f8b3703e2b0d7c1a
bastndev <gohit24@gmail.com>
2023-12-07 15:20:28 -0500

    :alien: Update `Learning`

--- lib/booleans.dart
+++ lib/booleans.dart
@@ -1,8 +1,16 @@
 //--- --- -TODO: bool (tru - false)
-void main() {
+/* void main() {
   bool meTrue = true;
   bool youFalse = false;
 
   print("Me : $meTrue");
   print("You : $youFalse");
+} */
+
+// --- --- -FIXME: Practicing (tru - false)
+
+void main() { 
+  
 }
+
+
