ef1176933ccdf5bc74b7e8094c6d3bb2c8a940f1
bastndev <gohit24@gmail.com>
2023-12-07 11:24:25 -0500

    :alien: Update `Learning`

--- lib/operations.dart
+++ lib/operations.dart
@@ -1,3 +1,12 @@
-void main() { 
-  
+void main() {
+  int a = 1;
+  int b = 2;
+
+  print("The result is: ${a + b}");
+  print("The result is: ${a - b}");
+  print("The result is: ${a * b}");
+  print("The result is: ${a / b}");
+
+  print("Hello, world!");
+  print("Hello, world!");
 }
