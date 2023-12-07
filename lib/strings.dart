187e7944ae413a19463b73d43fd06a14897ae43a
bastndev <gohit24@gmail.com>
2023-12-07 17:01:36 -0500

    :alien: Update `Learning`

--- lib/strings.dart
+++ lib/strings.dart
@@ -21,11 +21,23 @@
  */
 
 // --- --- -TODO: end
-void main() {
+/* void main() {
   String name = "Bastian";
 
   bool me = name.endsWith("ian");
   bool me2 = name.endsWith("ba");
+
+  print("You name contains Ba?: $me");
+  print("You name contains Ba?: $me2");
+}
+ */
+
+// --- --- -TODO: Start
+void main() {
+  String name = "Bastian";
+
+  bool me = name.startsWith("ian");
+  bool me2 = name.startsWith("Ba");
 
   print("You name contains Ba?: $me");
   print("You name contains Ba?: $me2");
