f9bc8148643dd22ede229be590360cabb9b76839
bastndev <gohit24@gmail.com>
2023-12-07 14:40:58 -0500

    :alien: Update `Learning`

---
+++ lib/methematicla_shortcuts.dart
@@ -0,0 +1,22 @@
+/* void main() {
+  num numbers = 10;
+  numbers = numbers + 1;
+
+  print(numbers);
+} */
+
+/* void main() {
+  num numbers = 10;
+  numbers += 2;
+
+  print(numbers);
+} */
+
+void main() {
+  int number1 = 10;
+  int number2 = 30;
+
+  number2 += 4;
+
+  print("the value number two is $number2 and number one is $number1");
+}
