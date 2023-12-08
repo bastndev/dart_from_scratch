6ea22d08cc53198ff2ad06286b6bf23f5569aa00
bastndev <gohit24@gmail.com>
2023-12-07 21:21:20 -0500

    :alien: Update `Learning`

--- lib/var_dynamic.dart
+++ lib/var_dynamic.dart
@@ -6,8 +6,23 @@
 
 } */
 
-void main() {
+/* void main() {
   var string = '10  Hello World';
 
   print(string);
+}
+ */
+
+void main() {
+  var noValue;
+  print(noValue);
+
+  noValue = 10;
+  print(noValue);
+
+  noValue = 'Hello';
+  print(noValue);
+
+  noValue = false;
+  print(noValue);
 }
