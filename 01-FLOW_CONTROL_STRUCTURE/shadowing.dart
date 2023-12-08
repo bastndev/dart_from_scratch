a877bb3881875c75a3d4dfac19ec148e5a106e53
bastndev <gohit24@gmail.com>
2023-12-08 18:21:19 -0500

    :alien: Update `Finish`

--- 01-FLOW_CONTROL_STRUCTURE/shadowing.dart
+++ 01-FLOW_CONTROL_STRUCTURE/shadowing.dart
@@ -1,11 +1,23 @@
-void main() {
+// --- Shadow
+/* void main() {
   int number = 0;
 
   if(true) {
     int number = 50;
     print("Shadow number: $number");
-    // ++number;
+    ++number;
   }
 
   print("Main number $number");
+}
+ */
+
+void main() {
+  String name = 'Bastian';
+
+  {
+    String name = 'Gohit';
+    print("My first name is: $name");
+  }
+  print("And my second name is: $name");
 }
