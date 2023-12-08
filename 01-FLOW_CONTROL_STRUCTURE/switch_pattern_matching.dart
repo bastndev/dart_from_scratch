a4a824023812b3b3b35a822043ddb6f56962502b
bastndev <gohit24@gmail.com>
2023-12-08 18:13:01 -0500

    :alien: Update `Finish`

--- 01-FLOW_CONTROL_STRUCTURE/switch_pattern_matching.dart
+++ 01-FLOW_CONTROL_STRUCTURE/switch_pattern_matching.dart
@@ -2,4 +2,17 @@ import 'dart:math';
 
 void main() {
   int number = Random().nextInt(20);
+
+  switch (number) {
+    case >= 0 && < 6:
+      print("First case: ");
+      print("0 <= $number < 6");
+      break;
+    case > 5 && <= 17:
+      print("Second case: ");
+      print("6 <= $number <= 17");
+      break;
+    default:
+      print("$number <= 19 ");
+  }
 }
