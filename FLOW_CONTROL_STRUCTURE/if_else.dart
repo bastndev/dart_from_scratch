7b44ce02ddfed476d3e29cdc8689bf0a6a451f7d
bastndev <gohit24@gmail.com>
2023-12-08 10:48:51 -0500

    :alien: Update `Finish`

---
+++ FLOW_CONTROL_STRUCTURE/if_else.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 1;
+
+  if (a == 5) {
+    print("Your number is equal to my number ");
+  } else {
+    print(
+        "Sorry bro I can't talk with you for what? \n$a is no equal to my number ");
+  }
+}
