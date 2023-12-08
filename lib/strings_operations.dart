d93b2d55981c286bb09a66fc008007594fa6bcaa
bastndev <gohit24@gmail.com>
2023-12-07 20:55:29 -0500

    :alien: Update `Learning`

--- lib/strings_operations.dart
+++ lib/strings_operations.dart
@@ -1,7 +1,10 @@
 void main() {
-  String language = 'Dart';
+  String language = 'Dart ';
   String count = '3.16.2';
 
   String result = language + count;
+  print(result);
+
+  result = 'Hello ' * 5;
   print(result);
 }
