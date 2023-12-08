f1c07525cbf53c178c80884bd3e91ac35c79e325
bastndev <gohit24@gmail.com>
2023-12-08 18:50:17 -0500

    :alien: Update `Finish`

---
+++ 01-FLOW_CONTROL_STRUCTURE/exercise_1.dart
@@ -0,0 +1,20 @@
+/* void main() {
+  double note1, note2, note3;
+
+  note1 = 5.5;
+  note2 = 4.0;
+  note3 = 7.0;
+
+  final sum = note1 + note2 + note3;
+  final media = sum / 3;
+  // print('The media is  ${media.toStringAsFixed(2)}');
+
+  if (media < 5) {
+    print("Sorry you disapproved whit  $media ");
+  } else if (media >= 5 && media <= 7) {
+    print("You approved whit $media");
+  } else {
+    print("Excellent whit $media");
+  }
+}
+ */
