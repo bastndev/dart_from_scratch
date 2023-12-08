e99f6b443148077b53933d9441a5d758d997e550
bastndev <gohit24@gmail.com>
2023-12-08 16:01:39 -0500

    :alien: Update `Finish`

--- 01-FLOW_CONTROL_STRUCTURE/switch.dart
+++ 01-FLOW_CONTROL_STRUCTURE/switch.dart
@@ -2,4 +2,28 @@ import 'dart:math';
 
 void main() {
   int random = Random().nextInt(6);
+
+  switch (random) {
+    case 0:
+      print('$random Monday');
+      break;
+    case 1:
+      print('$random Tuesday');
+      break;
+    case 2:
+      print('$random Wednesday');
+      break;
+    case 3:
+      print('$random Thursday');
+      break;
+    case 4:
+      print('$random Friday');
+      break;
+    case 5:
+      print('$random Saturday');
+      break;
+    case 6:
+      print('$random Sunday');
+      break;
+  }
 }
