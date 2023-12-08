8c0bb2bfe59b79902bac43fdc2e577cba48b9abb
bastndev <gohit24@gmail.com>
2023-12-07 21:05:40 -0500

    :alien: Update `Learning`

---
+++ lib/interpolation_strings.dart
@@ -0,0 +1,9 @@
+// --- -TODO: Option 1s
+void main() {
+  int a = 5;
+  int b = 10;
+
+  String operationSum =
+      a.toString() + ' + ' + b.toString() + ' = ' + (a + b).toString();
+  print(operationSum);
+}
