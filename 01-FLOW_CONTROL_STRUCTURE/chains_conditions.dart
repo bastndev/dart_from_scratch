cc78ba4c7c02516719667638b4b6fe4c083b7dc3
bastndev <gohit24@gmail.com>
2023-12-08 14:56:01 -0500

    :alien: Update `Finish`

--- 01-FLOW_CONTROL_STRUCTURE/chains_conditions.dart
+++ 01-FLOW_CONTROL_STRUCTURE/chains_conditions.dart
@@ -11,9 +11,15 @@
 import 'dart:math';
 
 void main() {
-  int random = Random().nextInt(20);
+  int random = Random().nextInt(20) + 1;
 
   if (random < 10) {
     print("$random is less than 10");
+  } else if (random >= 10 && random < 15) {
+    print("$random is more than 10 but less than 15");
+  } else if (random > 14 && random <= 19) {
+    print("$random is more than 15 but less than 19");
+  } else {
+    print("$random is 20");
   }
 }
