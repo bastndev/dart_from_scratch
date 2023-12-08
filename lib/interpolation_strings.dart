3286dd965d46019bc9b3129b4b293a4d68ad76ea
bastndev <gohit24@gmail.com>
2023-12-07 21:07:04 -0500

    :alien: Update `Learning`

--- lib/interpolation_strings.dart
+++ lib/interpolation_strings.dart
@@ -1,9 +1,18 @@
-// --- -TODO: Option 1s
-void main() {
+// --- -TODO: interpolation 1
+/* void main() {
   int a = 5;
   int b = 10;
 
   String operationSum =
       a.toString() + ' + ' + b.toString() + ' = ' + (a + b).toString();
+  print(operationSum);
+} */
+
+// --- -TODO: interpolation 2
+void main() {
+  int a = 5;
+  int b = 10;
+
+  String operationSum = '$a + $b = ${a + b}';
   print(operationSum);
 }
