fd14ce7d53a2a5c1476747c81ab33bd7bfc57a0d
bastndev <gohit24@gmail.com>
2023-12-08 09:40:17 -0500

    :alien: Update `Learning`

--- lib/null_safety.dart
+++ lib/null_safety.dart
@@ -14,10 +14,17 @@
   print(parsed! + 5);
 } */
 
-// --- --- -TODO: null safety
-void main() {
+// --- --- -TODO: null safety parsed
+/* void main() {
   String stringNumber = '10.5';
 
   int? parsed = int.tryParse(stringNumber)?? 0;
+  print("Hello null safety \n${parsed + 5}");
+} */
+
+void main() {
+  String stringNumber = '50';
+
+  int? parsed = int.tryParse(stringNumber) ?? 0;
   print("Hello null safety \n${parsed + 5}");
 }
