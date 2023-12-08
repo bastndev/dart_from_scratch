c87cd28cac97d743f9b48a99369e71d713571e62
bastndev <gohit24@gmail.com>
2023-12-08 11:23:45 -0500

    :alien: Update `Finish`

--- FLOW_CONTROL_STRUCTURE/practice_if_else.dart
+++ FLOW_CONTROL_STRUCTURE/practice_if_else.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-void main() {
+/* void main() {
   stdout.write('Introduce a number: ');
   final input = stdin.readLineSync() ?? '';
   final number = int.tryParse(input);
@@ -9,5 +9,19 @@ void main() {
     print("Please enter a number $input WTF");
   } else {
     print("You enter the number: $number");
+  }
+} */
+
+void main() {
+  stdout.write('Introduce a number: ');
+  final input = stdin.readLineSync() ?? '';
+  final number = int.tryParse(input);
+
+  if (number == null) {
+    print("Please enter a number $input WTF");
+  } else if (number <= 5) {
+    print("$number is equal to 5 or less");
+  } else {
+    print("$number is greater than 5 ++");
   }
 }
