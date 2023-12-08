776a2cbe2b6298e357d10fa44be88bda5504a1da
bastndev <gohit24@gmail.com>
2023-12-08 15:36:26 -0500

    :alien: Update `Finish`

---
+++ 01-FLOW_CONTROL_STRUCTURE/ternary_operator.dart
@@ -0,0 +1,12 @@
+// --- --- -FIXME: Before
+/* void main() {
+  int number = 5;
+
+  if (number % 2 == 0) {
+    print("The $number is even ");
+  } else {
+    print("The $number is odd ");
+  }
+} */
+
+// --- --- -FIXME: Before
