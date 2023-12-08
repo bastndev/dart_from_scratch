3b55b66e2f353f6704bda5e75f24004930494d57
bastndev <gohit24@gmail.com>
2023-12-08 08:51:31 -0500

    :alien: Update `Learning`

---
+++ lib/parseo.dart
@@ -0,0 +1,8 @@
+// --- --- -- --- Change of int to String.
+void main() { 
+  final intNumber = 10;
+  final doubleNumber = 5.12;
+
+  print(intNumber.toString());
+  print(doubleNumber.toString());
+}
