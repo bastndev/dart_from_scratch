e10e133a800d7dd54c0a4df63c8c61faa208eb38
bastndev <gohit24@gmail.com>
2023-12-08 08:33:08 -0500

    :alien: Update `Learning`

---
+++ lib/final_const.dart
@@ -0,0 +1,12 @@
+// both prevent a variable from being reassigned or rewritten
+// -TODO: final - const . cannot be reassigned 
+/* void main() { 
+  final number = 10;
+
+  number = 14;
+
+  const numbers = 11;
+  numbers = 12;
+} */
+
+// -TODO: final - const . in list
