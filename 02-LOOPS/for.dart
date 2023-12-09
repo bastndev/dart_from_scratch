6909f9b39b11ff13aa1b989216f948a4e524e890
bastndev <gohit24@gmail.com>
2023-12-09 08:56:15 -0500

    :arrow_up: Update `new`

---
+++ 02-LOOPS/for.dart
@@ -0,0 +1,13 @@
+// --- --- --- -TODO: for loop
+/* void main() {
+  for (int i = 0; 1 < 10; i++) {
+    print(i);
+  }
+}
+ */
+
+void main() {
+  for (int i = 0; i < 10; i += 2) {
+    print(i);
+  }
+}
