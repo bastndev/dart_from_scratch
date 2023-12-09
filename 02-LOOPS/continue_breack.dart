e5e4e9e809d852edee0670e87cc75c183f349ad1
bastndev <gohit24@gmail.com>
2023-12-09 09:46:27 -0500

    :arrow_up: Update `new`

--- 02-LOOPS/continue_breack.dart
+++ 02-LOOPS/continue_breack.dart
@@ -5,6 +5,22 @@
 } */
 
 //--- --- -FIXME: Practice continue and break
-void main() { 
-  
+void main() {
+  String phrase = 'Hello my name is Bastian zombie';
+  int length = phrase.length;
+  String newString = '';
+
+  for (int i = 0; i < length; i++) {
+    final char = phrase[i];
+    if (char == 'e' || char == 'z') {
+      print("Salting the loop with $char...");
+      continue;
+    } else if (char == 'Bastian') {
+      print("$char has been found!");
+      break;
+    } else {
+      newString += char;
+    }
+  }
+  print(newString);
 }
