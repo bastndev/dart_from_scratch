8a81575e81cfd9e3ef03606c446d0b03d07e5d0e
bastndev <gohit24@gmail.com>
2023-12-09 10:25:58 -0500

    :arrow_up: Update `new`

--- 02-LOOPS/do_while.dart
+++ 02-LOOPS/do_while.dart
@@ -1,8 +1,17 @@
-void main() { 
+/* void main() { 
   int limit = 10;
   int number = 0;
 
   do {
     print(++number);
   } while (number < limit);
+} */
+
+void main() { 
+  int limit = 10;
+  int number = 0;
+
+  do {
+    print("(Do while) always runs once");
+  } while (false);
 }
