b532f2824c6f80309697f36f193aeed029a829ef
bastndev <gohit24@gmail.com>
2023-12-09 10:19:09 -0500

    :arrow_up: Update `new`

--- 02-LOOPS/while.dart
+++ 02-LOOPS/while.dart
@@ -1,9 +1,21 @@
 // --- --- -TODO:
-void main() {
+/* void main() {
   int limit = 10;
   int number = 0;
 
   while (number < limit) {
     print(++number);
+  }
+} */
+
+void main() {
+  int limit = 10;
+  // int number = 0;
+
+  while (true) {
+    if (limit < 6) {
+      break;
+    }
+    print(--limit);
   }
 }
