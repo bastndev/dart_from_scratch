b15670b0f33d93120b585d9e9319711e8707193c
bastndev <gohit24@gmail.com>
2023-12-08 22:00:18 -0500

    :alien: Update `Finish`

--- 01-FLOW_CONTROL_STRUCTURE/exercise_3.dart
+++ 01-FLOW_CONTROL_STRUCTURE/exercise_3.dart
@@ -76,24 +76,23 @@ void main() {
   stdout.write('Enter fist number: ');
   final numAinput = stdin.readLineSync() ?? '';
   final numA = int.tryParse(numAinput);
-  if(numA == null) {
+  if (numA == null) {
     print("Enter one number pls: ");
     return;
   }
-
-    stdout.write('Enter second number: ');
-    final numBinput = stdin.readLineSync() ?? '';
-    final numB = int.tryParse(numBinput);
-    if (numB != null) {
-      if (numA == numB) {
-        print("Numbers are equals");
-      } else {
-        final stt = numA > numB
-            ? 'The number A ($numA) is most that number B ($numB)'
-            : 'The number B ($numB) is most that number A ($numA)';
-        print(stt);
-      }
-    } else {
-      print("Enter god the second number: ");
-    }
+  stdout.write('Enter second number: ');
+  final numBinput = stdin.readLineSync() ?? '';
+  final numB = int.tryParse(numBinput);
+  if (numB == null) {
+    print("Enter second number pls: ");
+    return;
   }
+  if (numA == numB) {
+    print("Numbers are equals");
+    return;
+  }
+  final stt = numA > numB
+      ? 'The number A ($numA) is most that number B ($numB)'
+      : 'The number B ($numB) is most that number A ($numA)';
+  print(stt);
+}
