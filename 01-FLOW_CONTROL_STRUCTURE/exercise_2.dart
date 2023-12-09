ecb8be5b7ec803d0a56e6c7143e11578bc989782
bastndev <gohit24@gmail.com>
2023-12-08 20:29:44 -0500

    :alien: Update `Finish`

--- 01-FLOW_CONTROL_STRUCTURE/exercise_2.dart
+++ 01-FLOW_CONTROL_STRUCTURE/exercise_2.dart
@@ -16,7 +16,7 @@
   }
 }*/
 
-void main() {
+/* void main() {
   String data = 'a';
 
   if (data == 'a' || data == 'e' || data == 'i' || data == 'o' || data == 'u') {
@@ -29,5 +29,45 @@ void main() {
     print("Is no vocal");
   } else {
     print("Enter only 1 world. pls");
+  }
+}
+ */
+
+/* void main() {
+  String data = 'a';
+  // double data1 = data.to;
+
+  if (data == 'a' || data == 'e' || data == 'i' || data == 'o' || data == 'u') {
+    print("is Vocal");
+  } else if (data != 'a' ||
+      data != 'e' ||
+      data != 'i' ||
+      data != 'o' ||
+      data != 'u') {
+    print("Is no vocal");
+  } else {
+    print("Enter only 1 world. pls");
+  }
+}
+ */
+
+//--- --- -TODO: Video
+import 'dart:io';
+
+void main() {
+  stdout.write('Introduce a lyric: ');
+  final input = (stdin.readLineSync() ?? '').toLowerCase();
+
+  if (input.length > 1) {
+    print("Error  change");
+  } else if (input.length == 1 &&
+      (input == 'a' ||
+          input == 'e' ||
+          input == 'i' ||
+          input == 'o' ||
+          input == 'u')) {
+    print("IS a vocal");
+  } else {
+    print("Is no vocal");
   }
 }
