5a253283cc4375aa79cb2aea36ddc5287af6b3b0
bastndev <gohit24@gmail.com>
2023-12-09 15:52:30 -0500

    :arrow_up: Update `new`

--- 02-LOOPS/nested_loops.dart
+++ 02-LOOPS/nested_loops.dart
@@ -1,4 +1,4 @@
-void main() {
+/* void main() {
   int iterations = 0;
 
   for (int i = 0; i < 10; i++) {
@@ -7,4 +7,14 @@ void main() {
     }
   }
   print(iterations);
+}
+ */
+
+void main() {
+  for (int i = 1; i < 10; i++) {
+    print("==== Table of $i ====");
+    for (int j = 1; j < 10; j++) {
+      print("$i x $j = ${i * j}");
+    }
+  }
 }
