b0e1fce4a8b2c3af9d1c65c647bc2c01287a2993
bastndev <gohit24@gmail.com>
2023-12-08 19:20:50 -0500

    :alien: Update `Finish`

---
+++ 01-FLOW_CONTROL_STRUCTURE/exercise_2.dart
@@ -0,0 +1,17 @@
+void main() {
+  String data = 'f';
+
+  if (data == 'a') {
+    print("is Vocal");
+  } else if (data == 'e') {
+    print("is Vocal");
+  } else if (data == 'i') {
+    print("is Vocal");
+  } else if (data == 'o') {
+    print("is Vocal");
+  } else if (data == 'u') {
+    print("is Vocal");
+  } else {
+    print("Enter only 1 world. pls");
+  }
+}
