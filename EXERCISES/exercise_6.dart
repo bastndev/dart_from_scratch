d061c996ce7e26f09d9f810a07cd655c20a5cd3d
bastndev <gohit24@gmail.com>
2023-12-09 20:59:22 -0500

    :arrow_up: Update `new`

---
+++ EXERCISES/exercise_6.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  int level = 10;
+  final char = '* ';
+
+  for (int i = 0; i < level; i++) {
+    int space = (level - 1) - i;
+    int chars = level - space;
+    for (int j = 0; j < space; j++) {
+      stdout.write(' ');
+    }
+    for (int j = 0; j < chars; j++) {
+      stdout.write(char);
+    }
+    print('');
+  }
+}
