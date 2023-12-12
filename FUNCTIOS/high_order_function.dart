07a64e655e7717733164bf8e17ea5029aded3ffb
bastndev <gohit24@gmail.com>
2023-12-11 22:35:48 -0500

    :arrow_up: Update `new`

--- FUNCTIOS/high_order_function.dart
+++ FUNCTIOS/high_order_function.dart
@@ -1,3 +1,21 @@
-void main() { 
-  
+void main() {
+  runner(countTo);
+
+  //Anonymous function
+  runner((){
+    countTo(20000000);
+  });
+}
+
+void runner(Function function) {
+  final start = DateTime.now().millisecondsSinceEpoch;
+
+  function();
+
+  final end = DateTime.now().millisecondsSinceEpoch;
+  print('Execution time ${end - start} ms');
+}
+
+void countTo([int number = 1000000]){
+  for(int i = 0; i < number; i++){}
 }
