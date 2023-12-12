75fd6515d17a465675da43d911f568480ac0291a
bastndev <gohit24@gmail.com>
2023-12-12 11:09:11 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/filter.dart
+++ 04-COLLECTIONS/filter.dart
@@ -1,3 +1,7 @@
-void main() { 
-  
+void main() {
+  final numbers = [5, 1, 2, 3, 4, 5, 10, 9, 20, 13, 14, 30, 12, 11];
+
+  final filter =
+      numbers.where((element) => element > 8 && element < 13).toList();
+  print(filter);
 }
