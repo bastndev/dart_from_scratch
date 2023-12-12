cb38cf1f0130c19de710368cd4041d2236bb538a
bastndev <gohit24@gmail.com>
2023-12-12 09:37:41 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/scroll_list.dart
+++ 04-COLLECTIONS/scroll_list.dart
@@ -1,8 +1,18 @@
-void main() {
+/* void main() {
   final numbers = [10, 20, 30];
 
   for (int i = 0; i < numbers.length; i++) {
     final number = numbers[i];
+    print(number);
+  }
+}
+ */
+
+// --- --- -TODO: Recommended
+void main() {
+  final numbers = [10, 20, 30];
+
+  for (int number in numbers) {
     print(number);
   }
 }
