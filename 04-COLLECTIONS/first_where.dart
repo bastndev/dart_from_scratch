16ccf5eef2111eaa0058957fe716282e7bdc4373
bastndev <gohit24@gmail.com>
2023-12-12 11:14:43 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/first_where.dart
+++ 04-COLLECTIONS/first_where.dart
@@ -4,3 +4,16 @@
   final number = numbers.firstWhere((element) => element == 4);
   print(number);
 } */
+
+void main() {
+  final numbers = [1, 2, 1, 3, 4, 2, 1];
+
+  final number = numbers.firstWhere((element) => element == 4);
+  print(number);
+
+  final noNumber = numbers.firstWhere(
+    (element) => element == 100,
+    orElse: () => -1,
+  );
+  print(noNumber);
+}
