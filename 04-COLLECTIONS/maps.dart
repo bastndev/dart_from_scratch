96cceab03edd8773dceee237d0a0edd4a1a5da84
bastndev <gohit24@gmail.com>
2023-12-12 14:55:09 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/maps.dart
+++ 04-COLLECTIONS/maps.dart
@@ -1,5 +1,16 @@
-void main() { 
-  Map<String, String> data = {
-    
+/* void main() {
+  Map<String, dynamic> data = {
+    'name': 'programming',
+    'web': 'http//bastndev.com',
+    'curse': 'dart',
+    'language': ['Dart'],
+    'localization': {
+      'web': 'http://udemy.com',
+      'android': 'apk Udemy',
+      'ios': 'app Udemy',
+    },
+    'price': null,
   };
+  print(data);
 }
+ */
