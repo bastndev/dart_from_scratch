a6ed8c3dadf305edf83abda3091e993e26922417
bastndev <gohit24@gmail.com>
2023-12-11 23:13:38 -0500

    :alie: Update `closures`

--- FUNCTIOS/closures.dart
+++ FUNCTIOS/closures.dart
@@ -1,6 +1,16 @@
-void main() {
+/* void main() {
   final closure = (name) {
     print("Ny name is: $name ");
   };
   closure('Lucas');
+}
+ */
+
+void main() {
+  final closure = (String name) {
+    print("Ny name is: $name ");
+    return name.length;
+  };
+  closure('Lucas');
+  print(closure);
 }
