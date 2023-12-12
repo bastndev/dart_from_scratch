8e0aba4f969088c5c5be2580c64443db58ca78ae
bastndev <gohit24@gmail.com>
2023-12-12 09:19:22 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/comparisons.dart
+++ 04-COLLECTIONS/comparisons.dart
@@ -1,6 +1,15 @@
+//--- --- true
 /* void main() {
   int a = 1;
   int b = 1;
 
   print(a == b);
 } */
+
+// --- --- false
+void main() {
+  List<int> a = [1, 2, 3, 4];
+  List<int> b = [1, 2, 3, 4];
+
+  print(a == b);
+}
