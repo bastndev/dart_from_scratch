7bd8d16fd7c081e881444c7abbf1ed1597304568
bastndev <gohit24@gmail.com>
2023-12-12 11:01:09 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/map.dart
+++ 04-COLLECTIONS/map.dart
@@ -1,5 +1,16 @@
 // -FIXME: The map method allows us to go through each element of a list and transform it into another type of data
 
-void main() { 
-  
+void main() {
+  final names = [
+    'flutter',
+    'google',
+    'apple',
+    'kotlin',
+    'vercel',
+    'react',
+    'javascript',
+  ];
+
+  final workCounts = names.map((e) => e.length).toList();
+  print(workCounts);
 }
