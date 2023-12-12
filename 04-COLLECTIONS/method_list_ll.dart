9b89207c33e226092e90606ad9522571710105a2
bastndev <gohit24@gmail.com>
2023-12-12 10:37:44 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/method_list_ll.dart
+++ 04-COLLECTIONS/method_list_ll.dart
@@ -12,3 +12,17 @@
   print(index);
 }
  */
+
+void main() {
+  final numbers = [1, 2, 3, 4, 3, 5, 6, 7];
+  // int index = numbers.indexOf(3);
+  // print(index);
+  // index = numbers.indexOf(100);
+  // print(index);
+
+  numbers.addAll([10, 20, 30]);
+
+  int index = numbers.indexWhere((element) => element > 5);
+  print(numbers);
+  print('$index corresponds to de the numbers ${numbers[index]} ');
+}
