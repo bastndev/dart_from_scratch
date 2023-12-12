b4959cc2c4d7dbd7c038995838a03521ed21f0ec
bastndev <gohit24@gmail.com>
2023-12-12 15:13:39 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/maps.dart
+++ 04-COLLECTIONS/maps.dart
@@ -14,3 +14,37 @@
   print(data);
 }
  */
+
+void main() {
+  Map<String, dynamic> data = {
+    'name': 'programming',
+    'web': 'http//bastndev.com',
+    'curse': 'dart',
+    'language': ['Dart'],
+    'localization': {
+      'web': 'http://udemy.com',
+      'android': 'apk Udemy',
+      'ios': 'app Udemy',
+    },
+    'price': null,
+  };
+
+  final containsName = data.containsKey('name');
+  print(containsName);
+
+  final containsProgramming = data.containsValue('programming');
+  print(containsProgramming);
+  
+
+  final contains = data.containsValue(['Dart']);
+  print(contains);
+
+  final name = data['name'];
+  final localization = data['localization'];
+
+  print(name);
+  print(localization['ios']);
+
+  final country = data['country'];
+  print(country);
+}
