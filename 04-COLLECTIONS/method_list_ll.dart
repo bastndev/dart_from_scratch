5b9949c6c7d2ffa51874e55a7636e0a12777d0b2
bastndev <gohit24@gmail.com>
2023-12-12 10:29:01 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/method_list_ll.dart
+++ 04-COLLECTIONS/method_list_ll.dart
@@ -1,6 +1,14 @@
-void main() {
+/* void main() {
   final numbers = [23, 2, 3, 4, 322, 5, 6];
 
   int index = numbers.indexOf(322);
   print(index);
+} */
+
+/* void main() {
+  final numbers = [23, 2, 3, 4, 322, 5, 6];
+
+  int index = numbers.indexOf(32);
+  print(index);
 }
+ */
