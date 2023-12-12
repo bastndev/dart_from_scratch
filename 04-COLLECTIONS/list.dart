65d7eabdd378cf5ffbbd139c97df11bbd231bb9c
bastndev <gohit24@gmail.com>
2023-12-12 09:08:39 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/list.dart
+++ 04-COLLECTIONS/list.dart
@@ -1,5 +1,12 @@
-void main() {
+/* void main() {
   List<int> numbers = [1, 2, 3, 4, 5, 6, 7];
 
   print(numbers);
+}
+ */
+
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7];
+
+  print(numbers[6]);
 }
