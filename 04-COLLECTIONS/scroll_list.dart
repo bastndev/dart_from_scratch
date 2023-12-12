47ca14463aba92cfbc90d411fd99b60baf02353a
bastndev <gohit24@gmail.com>
2023-12-12 09:42:21 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/scroll_list.dart
+++ 04-COLLECTIONS/scroll_list.dart
@@ -8,11 +8,21 @@
 }
  */
 
-// --- --- -TODO: Recommended
-void main() {
+// --- --- -TODO: Recommended for in
+/* void main() {
   final numbers = [10, 20, 30];
 
   for (int number in numbers) {
     print(number);
   }
+}
+ */
+
+// --- --- --- -TODO: forEach function
+void main() {
+  final numbers = [10, 20, 30];
+
+  numbers.forEach((element) {
+    print(element);
+  });
 }
