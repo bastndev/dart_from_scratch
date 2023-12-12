6b672c0b54a02d1bb203c9b801d68423dde547ef
bastndev <gohit24@gmail.com>
2023-12-11 22:01:58 -0500

    :arrow_up: Update `new`

--- FUNCTIOS/named_parameters.dart
+++ FUNCTIOS/named_parameters.dart
@@ -1,3 +1,20 @@
-void main() { 
-  
+void main() {
+  String str = 'flutter';
+  String reverse = reverseString(str: str);
+  print(reverse);
+}
+
+reverseString({required String str, int maxReverseLength = 5}) {
+  int length = str.length;
+  String reverse = '';
+  int count = 0;
+  for (int i = length - 1; i >= 0; i--) {
+    count++;
+    final char = str[i];
+    reverse += char;
+    if (count == maxReverseLength) {
+      return reverse;
+    }
+  }
+  return reverse;
 }
