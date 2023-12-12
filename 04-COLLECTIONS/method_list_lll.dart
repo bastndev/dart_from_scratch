02fe0f16d1d1d29a38acb27ffb7163c26a15be15
bastndev <gohit24@gmail.com>
2023-12-12 10:48:19 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/method_list_lll.dart
+++ 04-COLLECTIONS/method_list_lll.dart
@@ -17,3 +17,16 @@
   numbers.sort((a, b) => b.compareTo(a),);
   print(numbers);
 } */
+
+/* void main() {
+  final numbers = [1, 6, 4, 3, 5, 2];
+
+  numbers.sort((a, b) => a > b ? 1 : -1);
+  print(numbers);
+} */
+void main() {
+  final numbers = [1, 6, 4, 3, 5, 2];
+
+  numbers.sort((a, b) => a < b ? 1 : -1);
+  print(numbers);
+}
