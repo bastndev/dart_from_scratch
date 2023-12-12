05cfb2ba8282e504e7bcaf10346f63fe91544f5c
bastndev <gohit24@gmail.com>
2023-12-12 09:21:19 -0500

    :arrow_up: Update `new`

--- 04-COLLECTIONS/comparisons.dart
+++ 04-COLLECTIONS/comparisons.dart
@@ -7,9 +7,17 @@
 } */
 
 // --- --- false
-void main() {
-  List<int> a = [1, 2, 3, 4];
-  List<int> b = [1, 2, 3, 4];
+/* void main() {
+  List<int> listA = [1, 2, 3, 4];
+  List<int> listB = [1, 2, 3, 4];
 
-  print(a == b);
+  print(listA == listB);
+}
+ */
+
+void main() {
+  List<int> listA = [1, 2, 3, 4];
+  List<int> listB = [1, 2, 3, 4];
+
+  print(listA == listB);
 }
