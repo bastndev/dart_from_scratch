e2df2d654232e12c76bfbc62ba84e711b58f2c7a
bastndev <gohit24@gmail.com>
2023-12-13 10:14:42 -0500

    :arrow_up: Update `new`

---
+++ 05-POO/privacity.dart
@@ -0,0 +1,20 @@
+class Product {
+  final String id;
+  final String name;
+  final List<String> suppliers;
+  num price;
+  int quantity;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.suppliers,
+    this.quantity = 0,
+    required this.price,
+  });
+
+}
+
+void main() { 
+  
+}
