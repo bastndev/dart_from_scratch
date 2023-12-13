2841635cc7c203e18b404d68c31fc3cd00eb6d60
bastndev <gohit24@gmail.com>
2023-12-13 10:39:07 -0500

    :arrow_up: Update `new`

--- 05-POO/tostring.dart
+++ 05-POO/tostring.dart
@@ -1,4 +1,19 @@
-void main() { 
-  
-  
+class Product {
+  final String id;
+  final String name;
+  final List<String> suppliers;
+  num price;
+  int quantity;
+
+  Product(
+      {required this.id,
+      required this.name,
+      required this.price,
+      this.quantity = 0,
+      required this.suppliers});
+}
+
+void main() {
+  final car = Product(name: 'BMW', price: 40000, suppliers: [], id: '1');
+  print(car);
 }
