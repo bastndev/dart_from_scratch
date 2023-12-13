b43ddfed9d1a756a2c6610bdeb9f09a02c9ee325
bastndev <gohit24@gmail.com>
2023-12-13 09:33:46 -0500

    :arrow_up: Update `new`

--- 05-POO/builder.dart
+++ 05-POO/builder.dart
@@ -1,9 +1,19 @@
 class Product {
+  final String id;
   final String name;
-  final num price;
+  final List<String> suppliers;
+  num price;
+  int quantity;
 
-  const Product(this.name, this.price);
+  Product({
+    required this.id,
+    required this.name,
+    required this.suppliers,
+    this.quantity = 0,
+    required this.price,
+  });
+
+  // Product(this.name, this.price);
 }
 
-void main() {
-}
+void main() {}
