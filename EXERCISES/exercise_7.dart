034e7fee946ca6ef94dccfaa8a1d30bed0061f22
bastndev <gohit24@gmail.com>
2023-12-12 21:31:40 -0500

    :arrow_up: Update `new`

--- EXERCISES/exercise_7.dart
+++ EXERCISES/exercise_7.dart
@@ -15,6 +15,26 @@ void main() {
       }
     }
   };
+  makeMapPrettier(data);
+}
 
-  print(data);
+String makeMapPrettier(Map<String, dynamic> map) {
+  final lineJump = '\n';
+  final openChar = '{$lineJump';
+  final closeChar = '$lineJump}';
+  final comma = ',';
+  final separator = ':';
+  final keys = map.keys;
+
+  String str = openChar;
+  for (final key in keys) {
+    final value = map[key];
+    if (value is Map) {
+    } else {
+      str += key + separator + value.toString() + comma + lineJump;
+    }
+  }
+  str += closeChar;
+  print(str);
+  return str;
 }
