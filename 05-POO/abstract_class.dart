055e21b106793f104ca4caf0df61a79b954da48b
bastndev <gohit24@gmail.com>
2023-12-13 18:56:07 -0500

    :arrow_up: Update `new`

---
+++ 05-POO/abstract_class.dart
@@ -0,0 +1,10 @@
+/* abstract class Product {
+  void method() {
+    print("I'am a method");
+  }
+}
+
+// NO instance - but - can be inherited or can be implemented
+void main() {}
+ */
+// --- --- -TODO: overwrite
