b4dc6e2d4e25cf1c406c7b69e3344813a4b0bd21
bastndev <gohit24@gmail.com>
2023-12-13 09:23:28 -0500

    :arrow_up: Update `new`

--- 05-POO/immutability.dart
+++ 05-POO/immutability.dart
@@ -1,8 +1,15 @@
-class Product {
+/* class Product {
   Product(this.name, this.price); // builder product
 
   String name; // variable
   num price; // variable
 }
 
-void main() {}
+void main() {} */
+
+class Product {
+  final String name;
+  final num price;
+
+  Product(this.name, this.price);
+}
