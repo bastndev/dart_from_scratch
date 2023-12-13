d17555a33d5dc40f0845e2f1048026bce06d3490
bastndev <gohit24@gmail.com>
2023-12-13 10:30:53 -0500

    :arrow_up: Update `new`

--- 05-POO/proof.dart
+++ 05-POO/proof.dart
@@ -1,7 +1,8 @@
 import 'privacy.dart';
 
-void main() { 
-  final proof = Product(id: 'id', name: 'name', price: 300 , suppliers: []);
+void main() {
+  final proof = Product(id: 'id', name: 'name', price: 300, suppliers: []);
   print(proof.quantity);
-
+  proof.quantity = 100;
+  print(proof.quantity);
 }
