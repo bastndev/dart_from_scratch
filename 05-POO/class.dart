e7c511610c2a0f63d9b94eac580652000c840ee0
bastndev <gohit24@gmail.com>
2023-12-12 22:16:50 -0500

    :arrow_up: Update `new`

--- 05-POO/class.dart
+++ 05-POO/class.dart
@@ -17,6 +17,25 @@
 } */
 
 // builder - constructor
+/* class Product {
+  String name;
+  num price;
+
+  Product(this.name, this.price);
+}
+
+void main() {
+  final pc = Product('MackBook', 1700);
+  final car = Product('BMW', 40000);
+  print('Name: ${car.name} Price: ${car.price}');
+  car.name = 'Toyota';
+  car.price = 200000;
+  print('Name: ${car.name} Price: ${car.price}');
+  print('Name: ${pc.name} Price: ${pc.price}');
+
+}
+ */
+
 class Product {
   String name;
   num price;
@@ -27,6 +46,13 @@ class Product {
 void main() {
   final pc = Product('MackBook', 1700);
   final car = Product('BMW', 40000);
-  print(car.name);
-  print(pc.price);
+
+  print('\u001b[34mName: \u001b[0m\u001b[32m${car.name}\u001b[0m \u001b[34mPrice: \u001b[0m\u001b[32m${car.price}\u001b[0m');
+  
+  car.name = 'Toyota';
+  car.price = 200000;
+  
+  print('\u001b[34mName: \u001b[0m\u001b[32m${car.name}\u001b[0m \u001b[34mPrice: \u001b[0m\u001b[32m${car.price}\u001b[0m');
+  
+  print('\u001b[34mName: \u001b[0m\u001b[32m${pc.name}\u001b[0m \u001b[34mPrice: \u001b[0m\u001b[32m${pc.price}\u001b[0m');
 }
