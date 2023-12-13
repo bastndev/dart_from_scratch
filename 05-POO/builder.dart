48b556893bd513c33d3636e626303d03c2ce4e15
bastndev <gohit24@gmail.com>
2023-12-13 18:15:02 -0500

    :arrow_up: Update `new`

--- 05-POO/builder.dart
+++ 05-POO/builder.dart
@@ -1,3 +1,37 @@
+/* class Product {
+  final String id;
+  final String name;
+  final List<String> suppliers;
+  num price;
+  int quantity;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.suppliers,
+    this.quantity = 0,
+    required this.price,
+  });
+
+}
+
+void main() {
+  final car = Product(name: 'BMW', suppliers: ['333'], price: 40000, id: '1');
+
+  final suppliers = car.suppliers;
+  suppliers.add('a');
+  print(suppliers);
+  print(car.suppliers);
+
+// Copy no replace or add
+  final suppliersCopy = [...suppliers];
+  suppliersCopy.add('b');
+  print(suppliers);
+  print(suppliersCopy);
+}
+ */
+
+//builded nombrado
 class Product {
   final String id;
   final String name;
@@ -13,7 +47,23 @@ class Product {
     required this.price,
   });
 
-  // Product(this.name, this.price);
+  Product.empty({
+    this.id = '',
+    this.name = '',
+    this.price = 0,
+    this.quantity = 0,
+    this.suppliers = const [],
+  });
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      id: map['id'],
+      quantity: map['quantity'],
+      name: map['name'],
+      price: map['price'],
+      suppliers: [],
+    );
+  }
 }
 
 void main() {
