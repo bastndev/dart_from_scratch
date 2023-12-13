ebe49f742926af6133cbc6c5a06d115c1d57b5a3
bastndev <gohit24@gmail.com>
2023-12-12 20:46:48 -0500

    :arrow_up: Update `new`

---
+++ EXERCISES/exercise_7.dart
@@ -0,0 +1,20 @@
+// --- --- --- -TODO: Recursion
+void main() {
+  final data = {
+    'name': 'Bastian',
+    'lastName': 'Gohit',
+    'country': 'Spain',
+    'courseInfo': {
+      'name': 'Dart 3.0',
+      'language': 'Dart',
+      'extraInfo': {
+        'availability': 'worldwide',
+        'language': 'Spanish',
+        'length': '16 hours',
+        'videosNumber': 255,
+      }
+    }
+  };
+
+  print(data);
+}
