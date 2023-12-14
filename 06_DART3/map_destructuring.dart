0c7ca02097a824f29ff4abf150aefbfa3df4a3a3
bastndev <gohit24@gmail.com>
2023-12-14 15:20:46 -0500

    :arrow_up: Update `new`

--- 06_DART3/map_destructuring.dart
+++ 06_DART3/map_destructuring.dart
@@ -1,8 +1,22 @@
+//--- --- -TODO: Map destructuring
 void main() {
   final map = {
     'name': 'Bastian',
     'age': 25,
-    'height': 1.70,
+    'country': 'peru',
     'language': ['english', 'spanish']
   };
+
+  var {
+    'name': name as String,
+    'age': age,
+    'country': country,
+    'language': [language1, language2] as List<String>
+  } = map;
+
+  print(name);
+  print(age);
+  print(country);
+  print(language1);
+  print(language2);
 }
