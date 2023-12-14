f1141e6e2c6987a5f63f8dc67d5e9f1d185bbfd6
bastndev <gohit24@gmail.com>
2023-12-14 15:37:37 -0500

    :arrow_up: Update `new`

---
+++ 06_DART3/if_case.dart
@@ -0,0 +1,8 @@
+void main() {
+  final list = [1, 2];
+
+  if (list case [int x, int y]) {
+    print("The list contains two elements: $x, $y");
+    print("The sun fo the list is: ${x + y}");
+  }
+}
