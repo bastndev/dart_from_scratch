1d1905cc6b863b58c2d5f734a833445524b66766
bastndev <gohit24@gmail.com>
2023-12-13 21:33:30 -0500

    :arrow_up: Update `new`

--- 05-POO/inheritances.dart
+++ 05-POO/inheritances.dart
@@ -5,19 +5,46 @@ abstract class Person {
 
   Person(this.id, this.name, this.age);
 
+  hello() {}
   @override
   String toString() {
     return 'Person(id: $id, name: $name, age: $age)';
   }
 }
 
+// inheritances
 class Student extends Person {
   List<String> subjects = [];
   bool repeater;
   Student(super.id, super.name, super.age, {this.repeater = false});
 
   @override
+  hello() {
+    return super.hello();
+  }
+  @override
   String toString() {
     return '${super.toString()} repeater: $repeater, subject: $subjects';
+  }
+}
+
+// si un clase padre tiene atributos hay que agrgalo tambien en us herenias
+//Implementation
+
+class Director implements Person {
+  @override
+  int age;
+
+  @override
+  String id;
+
+  @override
+  String name;
+
+  Director(this.age, this.id, this.name);
+  
+  @override
+  hello() {
+    throw UnimplementedError();
   }
 }
