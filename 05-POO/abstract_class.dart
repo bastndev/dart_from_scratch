66a32385b6df82fdacebd518d78456273595af09
bastndev <gohit24@gmail.com>
2023-12-13 19:04:48 -0500

    :arrow_up: Update `new`

--- 05-POO/abstract_class.dart
+++ 05-POO/abstract_class.dart
@@ -7,4 +7,35 @@
 // NO instance - but - can be inherited or can be implemented
 void main() {}
  */
+
 // --- --- -TODO: overwrite
+abstract class Product {
+  void method() {
+    print("I'am a method");
+  }
+}
+
+class Car extends Product {
+  @override
+  void method() {
+    print("I'am a Car!");
+  }
+}
+
+class Dog extends Product {
+  // @override
+  // void method() {
+  //   print("I'am a dog!");
+  // }
+}
+
+// NO instance - but - can be inherited or can be implemented
+void main() {
+  final car = Car();
+  final dog = Dog();
+
+  List<Product> products = [car, dog];
+  for (final product in products) {
+    product.method();
+  }
+}
