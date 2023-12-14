ef003c9bdb44854c8de463311549812377ad5034
bastndev <gohit24@gmail.com>
2023-12-14 13:22:21 -0500

    :arrow_up: Update `new`

--- 05-POO/modifier.dart
+++ 05-POO/modifier.dart
@@ -1,7 +1,17 @@
-abstract class A{
+/* abstract class A{}
 
-}
 class B extends A{}
+class C implements A{}
+
+void main() { 
+} */
+
+//abstract inheritances 
+base class A {}
+
+base class B extends A{}
+final class C implements A{}
+sealed class D extends A{}
 
 void main() { 
   
