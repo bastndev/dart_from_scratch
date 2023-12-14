4c141997b3ac033aa3eb27bf5692fecfa958c164
bastndev <gohit24@gmail.com>
2023-12-13 22:14:45 -0500

    :arrow_up: Update `new`

---
+++ 05-POO/enum.dart
@@ -0,0 +1,17 @@
+enum WeekDay {
+  monday,
+  tuesdays,
+  wednesdays,
+  thursday,
+  friday,
+  saturday,
+  sunday;
+}
+
+void main() {
+  final week = WeekDay.monday;
+  print(week);
+  print(week.name);
+}
+
+// improved enums 
