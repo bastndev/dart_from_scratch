5b97e21f68001c92144c9b48a93f901d5da257db
bastndev <gohit24@gmail.com>
2023-12-14 15:42:38 -0500

    :arrow_up: Update `new`

--- 06_DART3/if_case.dart
+++ 06_DART3/if_case.dart
@@ -1,8 +1,13 @@
+//--- --- --- -TODO: Validate to Json
 void main() {
-  final list = [1, 2];
+  // final list = [1, 2];
+  final list = [1, 2, 3];
+  // final list = ['1', 2,3];
 
   if (list case [int x, int y]) {
-    print("The list contains two elements: $x, $y");
-    print("The sun fo the list is: ${x + y}");
+    print("The list contains 2 elements: $x, $y");
+    // print("The sun fo the list is: ${x + y}");
+  } else if (list case [int x, int y, int z]) {
+    print("The list contains 3 elements($x, $y, and $z)");
   }
 }
