984da1cdca0f217261d34c38526e3dc3f1f2f420
bastndev <gohit24@gmail.com>
2023-12-14 14:20:20 -0500

    :arrow_up: Update `new`

--- EXERCISES/exercise_9.dart
+++ EXERCISES/exercise_9.dart
@@ -1,4 +1,4 @@
-// --- --- --- -TODO: Recursion
+// --- --- --- -TODO: Extension of the class Map
 void main() {
   final data = {
     'name': 'Bastian',
@@ -15,42 +15,44 @@ void main() {
       }
     }
   };
-  print(makeMapPrettier(data));
+  print(data.makeMapPrettier());
 }
 
-String makeMapPrettier(Map<dynamic, dynamic> map, [int level = 1]) {
-  final spaces = '  ';
-  final lineJump = '\n';
-  final separator = ':';
-  final keys = map.keys;
-  final comma = ',';
+extension MapPrettierExtension on Map<dynamic, dynamic> {
+  String makeMapPrettier([int level = 1]) {
+    final spaces = '  ';
+    final lineJump = '\n';
+    final separator = ':';
+    final keys = this.keys;
+    final comma = ',';
 
-  final green = '\u001b[33m';
-  final yellow = '\u001b[35m';
-  final reset = '\u001b[0m';
-  final blue = '\u001b[36m';
-  final openChar = '$blue{$lineJump$reset';
-  final closeChar = '$lineJump${spaces * (level - 1)}$blue}$reset';
+    final green = '\u001b[33m';
+    final yellow = '\u001b[35m';
+    final reset = '\u001b[0m';
+    final blue = '\u001b[36m';
+    final openChar = '$blue{$lineJump$reset';
+    final closeChar = '$lineJump${spaces * (level - 1)}$blue}$reset';
 
-  String str = openChar;
-  for (final key in keys) {
-    final value = map[key];
-    if (value is Map) {
-      str += spaces * level + yellow + key + separator + reset;
-      str += makeMapPrettier(value, level + 1);
-    } else {
-      str += spaces * level +
-          yellow +
-          key +
-          separator +
-          reset +
-          green +
-          value.toString() +
-          reset +
-          comma +
-          lineJump;
+    String str = openChar;
+    for (final key in keys) {
+      final value = this[key];
+      if (value is Map) {
+        str += spaces * level + yellow + key + separator + reset;
+        str += value.makeMapPrettier(level + 1);
+      } else {
+        str += spaces * level +
+            yellow +
+            key +
+            separator +
+            reset +
+            green +
+            value.toString() +
+            reset +
+            comma +
+            lineJump;
+      }
     }
+    str += closeChar;
+    return str;
   }
-  str += closeChar;
-  return str;
 }
