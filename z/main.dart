1b763515a1bff3e98cbd806008ae4ef095bb5b5c
bastndev <gohit24@gmail.com>
2023-12-14 09:51:43 -0500

    :arrow_up: Update `new`

---
+++ z/main.dart
@@ -0,0 +1,15 @@
+void main() {
+  bool finishProgram = false;
+
+  do {
+    print('=========== StoreHouse Program ==============');
+    print('\t1 Products List');
+    print('\t2 Add Products');
+    print('\t3 Delete Products');
+    print('\t4 Search Products');
+    print('\t5 Add quantity to Products');
+    print('\t6 Subtract quantity to Products');
+    print('\t7 End program');
+    print('choose an option \r');
+  } while (!finishProgram);
+}
