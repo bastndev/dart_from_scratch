2b60a2b7464b5d5fcea02e5ba7e6a3c6ef6cb5e2
bastndev <gohit24@gmail.com>
2023-12-14 21:46:55 -0500

    :arrow_up: Update `new`

--- 07-GENERIC/customprint_generic.dart
+++ 07-GENERIC/customprint_generic.dart
@@ -11,10 +11,23 @@ enum Color {
 void main() {}
 
 class CustomPrint {
+  static const reset = '\u001B[0m';
+
   final String text;
   final PrintStyle? style;
 
-  CustomPrint(this.text, {this.style});
+  CustomPrint({required this.text, this.style});
+
+  String _createSpace() {
+    String spaces = '';
+    for (int i = 0; i < style!.separation!.property; i++) {}
+    return text.split('').join(spaces);
+  }
+
+  void write() {
+    String str = '';
+    String colorCode = style?.color?.property.code ?? '';
+  }
 }
 
 class PrintStyle {
