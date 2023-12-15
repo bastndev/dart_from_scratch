da5080cafe5221af1a93b1c056264e1a0bf87f5c
bastndev <gohit24@gmail.com>
2023-12-15 10:54:05 -0500

    :arrow_up: Update `new`

---
+++ 09-PRACTICE/class.dart
@@ -0,0 +1,21 @@
+/* class Music {
+  final String artName;
+  final String nameSong;
+  final num reproductions;
+
+  Music({
+    required this.artName,
+    required this.nameSong,
+    required this.reproductions,
+  });
+}
+
+void main() {
+  final myMusic = Music(
+      artName: 'Juan Gabriel', nameSong: 'Despasito', reproductions: 111.300);
+
+  print(myMusic.artName);
+  print(myMusic.nameSong);
+  print(myMusic.reproductions);
+}
+ */
