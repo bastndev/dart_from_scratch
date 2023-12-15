30b5668a4b19e611451fcca46e072968a37599c7
bastndev <gohit24@gmail.com>
2023-12-15 10:55:16 -0500

    :arrow_up: Update `new`

--- 09-PRACTICE/class.dart
+++ 09-PRACTICE/class.dart
@@ -19,3 +19,27 @@ void main() {
   print(myMusic.reproductions);
 }
  */
+
+class Music {
+  final String artName;
+  final String nameSong;
+  final num reproductions;
+
+  Music({
+    required this.artName,
+    required this.nameSong,
+    required this.reproductions,
+  });
+
+  @override
+  String toString() {
+    return 'Music{artName: $artName, nameSong: $nameSong, reproductions: $reproductions}';
+  }
+}
+
+void main() {
+  final myMusic = Music(
+      artName: 'Juan Gabriel', nameSong: 'Despacito', reproductions: 111300);
+
+  print(myMusic);
+}
