6db1ee71f08559f237499c6fac100df5ea6240cc
bastndev <gohit24@gmail.com>
2023-12-15 10:40:23 -0500

    :arrow_up: Update `new`

--- 08-EXEPTIONS/custom_exception.dart
+++ 08-EXEPTIONS/custom_exception.dart
@@ -1,4 +1,4 @@
-class NotAllowedDivision implements Exception {
+/* class NotAllowedDivision implements Exception {
   final String message;
   final int providerNumber;
 
@@ -21,6 +21,49 @@ void main() {
     print('FormatException $e');
   } on NotAllowedDivision catch (e) {
     print(e);
+  } finally {
+    print('finally');
+  }
+}
+
+int parse(String str, int a) {
+  try {
+    if (a >= 5) {
+      throw NotAllowedDivision(
+          message: 'cannot be divided by a number equal to or greater than 5',
+          providerNumber: a);
+    }
+    return int.parse(str) ~/ a;
+  } catch (_) {
+    rethrow;
+  }
+}
+ */
+
+class NotAllowedDivision implements Exception {
+  final String message;
+  final int providerNumber;
+
+  const NotAllowedDivision(
+      {required this.message, required this.providerNumber});
+
+  @override
+  String toString() {
+    return 'Message: $message, Provider: $providerNumber';
+  }
+}
+
+void main() {
+  try {
+    final result = parse('3', 9);
+    print(result);
+  } on IntegerDivisionByZeroException catch (e) {
+    print("one as ocurred IntegerDivisionByZeroException $e");
+  } on FormatException catch (e) {
+    print('FormatException $e');
+  } on NotAllowedDivision catch (e) {
+    print(e.providerNumber);
+    print(e.message);
   } finally {
     print('finally');
   }
