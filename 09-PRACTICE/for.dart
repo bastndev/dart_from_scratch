6c91acd197995b69bc6bbfaa14ba09bdae1bd795
bastndev <gohit24@gmail.com>
2023-12-16 14:54:19 -0500

    :arrow_up: Update `new`

--- 09-PRACTICE/for.dart
+++ 09-PRACTICE/for.dart
@@ -13,3 +13,13 @@
   }
 }
  */
+
+void main() { 
+  for (int i = 0; i <10 ; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }else{
+      print('odd');
+    }
+  }
+}
--- 09-PRACTICE/while.dart
+++
@@ -1,9 +0,0 @@
-void main() { 
-  for (int i = 0; i <10 ; i++) {
-    if (i % 2 == 0) {
-      print(i);
-    }else{
-      print('odd');
-    }
-  }
-}
