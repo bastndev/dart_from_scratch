21732cfcf7a6dc719dddda717b480470e5ee9104
bastndev <gohit24@gmail.com>
2023-12-16 14:44:17 -0500

    :arrow_up: Update `new`

--- 09-PRACTICE/for.dart
+++ 09-PRACTICE/for.dart
@@ -1,5 +1,14 @@
-void main() {
+/* void main() {
   for (int i = 0; i <= 12; i++) {
     print("1 x ${i * 1} = ${i * 1}");
+  }
+}
+ */
+
+void main() {
+  for (int tabla = 1; tabla <= 12; tabla++) {
+    for (int i = 1; i <= 12; i++) {
+      print("$tabla x $i = ${tabla * i}");
+    }
   }
 }
