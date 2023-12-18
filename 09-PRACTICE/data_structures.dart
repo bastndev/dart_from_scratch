6056f6e8e3b590f9cfdc96b5d8a0c35ebdf85682
bastndev <gohit24@gmail.com>
2023-12-17 22:30:15 -0500

    :arrow_up: Update `new`

--- 09-PRACTICE/data_structures.dart
+++ 09-PRACTICE/data_structures.dart
@@ -3,8 +3,18 @@
   print(list);
 }
  */
+
+/* void main() {
+  final list = ["The chicken", " 17 years","The Reminder "];
+  final lists = list.length;
+  print(lists);
+} */
 /* void main() {
   final list = ["The chicken", " 17 years","The Reminder "];
   // final lists = list.length;
   print(list[2]);
 } */
+void main() {
+  final list = ["The chicken", " 17 years","The Reminder "];
+  print(list[list.length - 1]);
+}
