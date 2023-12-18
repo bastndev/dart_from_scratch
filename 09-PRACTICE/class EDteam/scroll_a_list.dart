044328267b4122002dbe04daa8dd55a6777a276c
bastndev <gohit24@gmail.com>
2023-12-17 23:24:24 -0500

    :arrow_up: Update `new`

---
+++ 09-PRACTICE/class EDteam/scroll_a_list.dart
@@ -0,0 +1,12 @@
+void main() {
+  final list = [];
+
+  list.add("Ella baila sola");
+  list.add("40 23");
+  list.add("a un estas en mis sueños");
+  list.add("Su calor");
+
+  for (int i = 0; i < list.length; i++) {
+    print(list[i]);
+  }
+}
