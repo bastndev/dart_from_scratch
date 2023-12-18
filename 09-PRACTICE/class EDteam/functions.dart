42a917db60bbc03299f4d9d31d868f40dfb71d14
bastndev <gohit24@gmail.com>
2023-12-18 09:53:15 -0500

    :arrow_up: Update `new`

---
+++ 09-PRACTICE/class EDteam/functions.dart
@@ -0,0 +1,18 @@
+/* void main() {
+  greet() {
+    print("hello ");
+  }
+
+  greet();
+}
+ */
+
+void main() {
+  greet(name) {
+    print("Hello $name Welcome");
+  }
+
+  greet("Juan");
+  greet("Lucas");
+  greet("Maria");
+}
