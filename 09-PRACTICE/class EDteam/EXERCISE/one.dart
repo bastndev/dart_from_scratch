0edc74c5f5c078c81f3f9c706deb866733ce7b80
bastndev <gohit24@gmail.com>
2023-12-18 10:47:01 -0500

    :arrow_up: Update `new`

--- 09-PRACTICE/class EDteam/EXERCISE/one.dart
+++ 09-PRACTICE/class EDteam/EXERCISE/one.dart
@@ -6,9 +6,39 @@ void main() {
     return b;
   }
 
-  final a = 1.90;
-  final b = 120.2;
+  final a = 110;
+  final b = 20;
   final c = 30;
+  final d = 430;
+  final e = 50;
+
+  final result1 = elderly(a, b);
+  final result2 = elderly(c, result1);
+  final result3 = elderly(d, result2);
+  final result4 = elderly(d, result3);
+  final result5 = elderly(e, result4);
+  print("The number most is: $result5");
+}
+
+
+
+
+
+
+
+
+
+/* void main() {
+  num elderly(num a, num b) {
+    if (a > b) {
+      return a;
+    }
+    return b;
+  }
+
+  final a = 90;
+  final b = 12;
+  final c = 310;
   final d = 40;
 
   final result1 = elderly(a, b);
@@ -16,4 +46,4 @@ void main() {
   final result3 = elderly(result1, result2);
 
   print("The largest number is: $result3");
-}
+} */
