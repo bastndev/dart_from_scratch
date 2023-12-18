540f3df114bc6c081dff80b73aba44d695c8a13c
bastndev <gohit24@gmail.com>
2023-12-17 22:14:17 -0500

    :arrow_up: Update `new`

--- 09-PRACTICE/atm.dart
+++ 09-PRACTICE/atm.dart
@@ -40,7 +40,7 @@ void main() {
 }
  */
 
-
+/* 
 import 'dart:io';
 
 final initialBalance = 1000;
@@ -84,6 +84,46 @@ void main() {
       } else {
         balance += input3;
         print("Your balance is now \$$balance");
+      }
+    }
+  }
+}
+ */
+
+import 'dart:io';
+
+void main() {
+  int balance = 1000;
+
+  while (true) {
+    stdout.write(
+        "You have now \$$balance | What operation should you perform?\n (1) Add\n (2) Withdraw\n (3) Exit: ");
+    final option = stdin.readLineSync() ?? '';
+    // final input  = int.tryParse(option);
+
+    if (option == '3') {
+      print("========== bye 👋 =========");
+      break;
+    }
+    if (option == '1') {
+      stdout.write("How much do you want to withdraw: ");
+      final option2 = stdin.readLineSync() ?? '';
+      final input2 = int.tryParse(option2);
+      if (input2 == null) {
+        print("Enter Number please");
+      } else {
+        balance += input2;
+        print("agregaste $input2");
+      }
+    } else if (option == '2') {
+      stdout.write("Cuanto quieres restitar rey: ");
+      final option3 = stdin.readLineSync() ?? '';
+      final input3 = int.tryParse(option3);
+      if (input3 == null) {
+        print("Enter Number please");
+      } else {
+        balance -= input3;
+        print("Retiraste $input3");
       }
     }
   }
