4a20e83a093170943107983dd9e9723f2cb6e125
bastndev <gohit24@gmail.com>
2023-12-18 10:51:46 -0500

    :arrow_up: Update `new`

--- 09-PRACTICE/atm.dart
+++ 09-PRACTICE/atm.dart
@@ -89,7 +89,7 @@ void main() {
   }
 }
  */
-
+/* 
 import 'dart:io';
 
 void main() {
@@ -124,6 +124,98 @@ void main() {
       } else {
         balance -= input3;
         print("Retiraste $input3");
+      }
+    }
+  }
+}
+ */
+
+/* import 'dart:io';
+
+void main() {
+  final initialBalance = 1000;
+  int balance = initialBalance;
+
+  while (true) {
+    stdout.write(
+        "You have now \$$balance | What operation should you perform?\n (1)add:\n (2)Withdraw:\n (3)Exit: ");
+    final option = stdin.readLineSync() ?? '';
+    final input = int.tryParse(option);
+
+    if (input == 3) {
+      print("========== bye 👋 =========");
+      break;
+    } else if (input == 2) {
+      stdout.write("How much do you want to withdraw: ");
+      final option2 = stdin.readLineSync() ?? '';
+      final input2 = int.tryParse(option2);
+
+      if (input2 == null || input2 > balance) {
+        print("You do not have enough balance :(");
+      } else {
+        balance -= input2;
+        print("Your balance is $balance");
+      }
+    } else if (input == 1) {
+      stdout.write("How much do you want Add: ");
+      final option3 = stdin.readLineSync() ?? '';
+      final input3 = int.tryParse(option3);
+
+      if (input3 == null) {
+        print("Enter Number please:");
+      } else {
+        balance += input3;
+        print("Your balance is now \$$balance");
+      }
+    }
+  }
+}
+ */
+
+
+import 'dart:io';
+
+final initialBalance = 1000;
+final exitOption = 3;
+
+void main() {
+  int balance = initialBalance;
+
+  while (true) {
+    stdout.write("You have now \$$balance | What operation should you perform?\n (1) Add\n (2) Withdraw\n (3) Exit: ");
+    
+    final option = stdin.readLineSync() ?? '';
+    final input = int.tryParse(option);
+
+    if (input == null || (input < 1 || input > exitOption)) {
+      print("Please enter a valid option (1, 2, or 3).");
+      continue;
+    }
+
+    if (input == exitOption) {
+      print("========== bye 👋 =========");
+      break;
+    } else if (input == 2) {
+      stdout.write("How much do you want to withdraw: ");
+      final option2 = stdin.readLineSync() ?? '';
+      final input2 = int.tryParse(option2);
+
+      if (input2 == null || input2 > balance) {
+        print("You do not have enough balance :(");
+      } else {
+        balance -= input2;
+        print("Your balance is $balance");
+      }
+    } else if (input == 1) {
+      stdout.write("How much do you want Add: ");
+      final option3 = stdin.readLineSync() ?? '';
+      final input3 = int.tryParse(option3);
+
+      if (input3 == null) {
+        print("Enter Number please:");
+      } else {
+        balance += input3;
+        print("Your balance is now \$$balance");
       }
     }
   }
