ae1cc15368e878267408541e9083241ef17b2e3b
bastndev <gohit24@gmail.com>
2023-12-18 10:07:19 -0500

    :arrow_up: Update `new`

--- 09-PRACTICE/class EDteam/functions.dart
+++ 09-PRACTICE/class EDteam/functions.dart
@@ -17,11 +17,18 @@
   greet("Maria");
 } */
 
-void main() {
+/* void main() {
   sum(a, b) {
     return a + b;
   }
 
   final result = sum(14, 14);
   print("The sun of the numbers is: $result");
-}
+} */
+
+/* void main() {
+  sum(a, b) => a + b;
+
+  final result = sum(14, 14);
+  print("The sun of the numbers is: $result");
+} */
