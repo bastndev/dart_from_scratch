02db9864e2f5e4b84f78df2bdfc7bbf42bc03d62
bastndev <gohit24@gmail.com>
2023-12-17 23:48:02 -0500

    :arrow_up: Update `new`

--- 09-PRACTICE/class EDteam/scroll_a_list.dart
+++ 09-PRACTICE/class EDteam/scroll_a_list.dart
@@ -41,3 +41,45 @@
   });
 }
  */
+
+// -FIXME: only one element
+/* void main() {
+  final list = [];
+
+  list.add("Ella baila sola");
+  list.add("40 23");
+  list.add("a un estas en mis sueños");
+  list.add("Su calor");
+
+  list.forEach((element)=> print(element));
+} */
+
+// --- --- -FIXME: Recommend for use
+
+/* void main() {
+  final list = [];
+
+  list.add("Ella baila sola");
+  list.add("40 23");
+  list.add("a un estas en mis sueños");
+  list.add("Su calor");
+
+  for (var element in list) {
+    print(element);
+  }
+}
+ */
+
+//--- --- -TODO: Practices
+
+void main() {
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  for (int number in numbers.toList()) {
+    if (number % 2 != 0) {
+      numbers.remove(number);
+    }
+  }
+
+  print(numbers);
+}
