444832a93668bdcdcb4b51319b583ca5deda1669
bastndev <gohit24@gmail.com>
2023-12-19 15:53:59 -0500

    :arrow_up: Update `new`

--- 11-FILE_MANAGEMENT/write_bytes.dart
+++ 11-FILE_MANAGEMENT/write_bytes.dart
@@ -1,4 +1,15 @@
-// --- FIXME: Audio and video are binary
-void main() { 
-  
+// --- -FIXME: Audio and video are binary
+import 'dart:io';
+
+void main() async {
+  const filesFolder = 'zz/';
+  const imagePath = '${filesFolder}cat.jpg';
+  const imageCopyPath = '${filesFolder}copy_cat.jpg';
+
+  final catFile = File(imagePath);
+
+  final bytes = await catFile.readAsBytes();
+
+  final catCopyFile = File(imageCopyPath);
+  catCopyFile.writeAsBytes(bytes);
 }
