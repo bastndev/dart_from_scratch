8323496c4be8a2d527052405186943f9e4d6a828
bastndev <gohit24@gmail.com>
2023-12-19 09:02:13 -0500

    :arrow_up: Update `new`

--- 10-FUTURE/future.dart
+++ 10-FUTURE/future.dart
@@ -11,3 +11,22 @@ void main() {
   print("3: Finish program");
 }
  */
+
+import 'dart:async';
+
+void main() {
+  print("1: Start the program");
+  // --- --- asynchronous 
+  scheduleMicrotask(() {
+    print("-: First microtask");
+  });
+  scheduleMicrotask(() {
+    print("--: Second microtask");
+  });
+  scheduleMicrotask(() {
+    print("---: Third microtask");
+  });
+  //--- The future does not begin until microtasks work
+  Future.value(10).then((value) => print("2: This is value is $value"));
+  print("3: Finish program");
+}
