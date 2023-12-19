06a889f2f0bf45830d30781a95aa840f9116d665
bastndev <gohit24@gmail.com>
2023-12-19 15:18:21 -0500

    :arrow_up: Update `new`

--- 11-FILE_MANAGEMENT/read_file.dart
+++ 11-FILE_MANAGEMENT/read_file.dart
@@ -1,7 +1,15 @@
 import 'dart:io';
 
-void main() async {
+/* void main() async {
   final file = File('zz/file1.txt');
   final result = await file.readAsBytes();
   print(String.fromCharCodes(result));
 }
+ */
+
+/* void main() async {
+  final file = File('zz/file1.txt');
+  final result = await file.readAsLines();
+  print(result);
+} */
+
