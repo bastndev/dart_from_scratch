0466774b38f42bfea4804933d18ca2727672bebb
bastndev <gohit24@gmail.com>
2023-12-19 10:02:26 -0500

    :arrow_up: Update `new`

---
+++ 10-FUTURE/async_problem.dart
@@ -0,0 +1,27 @@
+void main() {
+  List<num> numbers = [];
+
+  Future.value(1).then((value) {
+    numbers.add(value);
+    Future.delayed(const Duration(milliseconds: 1000), () {
+      return 5;
+    }).then((value) {
+      numbers.add(value);
+      Future.value(3).then((value) {
+        numbers.add(value);
+        Future.microtask(() {
+          numbers.add(1);
+          print(sum(numbers));
+        });
+      });
+    });
+  });
+}
+
+num sum(List<num> numbers) {
+  num count = 0;
+  for (final n in numbers) {
+    count += n;
+  }
+  return count;
+}
