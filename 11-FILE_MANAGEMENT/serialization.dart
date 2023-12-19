117dcd367d51e63b238d8465b566b3d47f01794b
bastndev <gohit24@gmail.com>
2023-12-19 18:53:05 -0500

    :arrow_up: Update `new`

--- 11-FILE_MANAGEMENT/serialization.dart
+++ 11-FILE_MANAGEMENT/serialization.dart
@@ -3,16 +3,64 @@
 
 import 'dart:convert';
 import 'dart:io';
-
+/* 
 void main() async {
   final file = File('zz/serialization.txt');
   final country = Country(nameEN: 'Andorra', nameES: 'Andorra', iso: 'AD');
+
 
   String str = await file.readAsString();
 
   print(Country.fromJson(str));
 
   file.writeAsString(country.toJson());
+}
+
+class Country {
+  final String nameEN;
+  final String nameES;
+  final String iso;
+
+  const Country(
+      {required this.nameEN, required this.nameES, required this.iso});
+
+  factory Country.fromList(List<String> list) {
+    return Country(nameEN: list.first, nameES: list[1], iso: list.last);
+  }
+
+  Map<String, dynamic> toMap() =>
+      {'nameEN': nameEN, 'nameES': nameES, 'iso': iso};
+
+  String toJson() => jsonEncode(toMap());
+
+  factory Country.fromMap(Map<String, dynamic> map) {
+    return Country(
+        nameEN: map['nameEN'], nameES: map['nameES'], iso: map['iso']);
+  }
+
+  factory Country.fromJson(String str) {
+    return Country.fromMap(jsonDecode(str));
+  }
+
+  @override
+  String toString() {
+    return 'nameEn: $nameEN, nameES: $nameES, iso: $iso\n';
+  }
+}
+ */
+
+void main() async {
+  final file = File('zz/serialization.txt');
+  final country = Country(nameEN: 'Andorra', nameES: 'Andorra', iso: 'AD');
+  final countries = [country, Country(nameEN: 'Perú', nameES: 'Peru', iso: 'PE')];
+
+  String str = await file.readAsString();
+
+  final decodedCountries = jsonDecode(str);
+
+  print(decodedCountries.map((e) => Country.fromJson(e)).toList()); 
+
+  file.writeAsString(jsonEncode(countries));
 }
 
 class Country {
