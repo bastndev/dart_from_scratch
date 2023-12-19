7736ae3ae8b6e96fa8733ce7d7b1388eedbeda4e
bastndev <gohit24@gmail.com>
2023-12-19 10:22:42 -0500

    :arrow_up: Update `new`

--- 10-FUTURE/chronometer.dart
+++ 10-FUTURE/chronometer.dart
@@ -1,3 +1,9 @@
-void main() { 
-  
+void main() async {
+  int seconds = 0;
+
+  while (seconds < 60) {
+    await Future.delayed(const Duration(milliseconds: 900));
+    seconds++;
+    print(seconds);
+  }
 }
