d524b9e25a8add10c11243bd8dea6c4612498349
bastndev <gohit24@gmail.com>
2023-12-19 09:09:54 -0500

    :arrow_up: Update `new`

--- 10-FUTURE/future.dart
+++ 10-FUTURE/future.dart
@@ -14,7 +14,7 @@ void main() {
 
 import 'dart:async';
 
-void main() {
+/* void main() {
   print("1: Start the program");
   // --- --- asynchronous 
   scheduleMicrotask(() {
@@ -27,6 +27,25 @@ void main() {
     print("---: Third microtask");
   });
   //--- The future does not begin until microtasks work
+  Future.value(10).then((value) => print("2: This is value is $value"));
+  print("3: Finish program");
+} */
+
+void main() {
+  print("1: Start the program");
+
+  scheduleMicrotask(() {
+    print("-: First microtask");
+  });
+  scheduleMicrotask(() {
+    print("--: Second microtask");
+  });
+  scheduleMicrotask(() {
+    print("---: Third microtask");
+  });
+
+  Future.microtask(() => Future.value(500).then(print));
+  Future.microtask(() => Future.value(900).then(print));
   Future.value(10).then((value) => print("2: This is value is $value"));
   print("3: Finish program");
 }
