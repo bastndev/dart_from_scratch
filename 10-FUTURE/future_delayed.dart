5d2015f2613e8be51d7b302c32d54ce67d32e0db
bastndev <gohit24@gmail.com>
2023-12-19 09:26:22 -0500

    :arrow_up: Update `new`

--- 10-FUTURE/future_delayed.dart
+++ 10-FUTURE/future_delayed.dart
@@ -1,3 +1,18 @@
-void main() { 
-  
+/* void main() {
+  Future.delayed(Duration(milliseconds: 900), () {
+    return 1;
+  }).then(print);
+} */
+
+/* void main() {
+  Future.delayed(Duration(milliseconds: 900), () {
+    return print("Hello");
+  }).then(print);
+}
+ */
+
+void main() {
+  Future.delayed(const Duration(milliseconds: 900)).then((_) {
+    return print("This will print within 1 second");
+  });
 }
