fbb426577dd9b70f8002c6106d11f7141cfc7add
bastndev <gohit24@gmail.com>
2023-12-19 11:17:28 -0500

    :arrow_up: Update `new`

--- 10-FUTURE/timeout.dart
+++ 10-FUTURE/timeout.dart
@@ -1,6 +1,10 @@
 void main() {
   const timeout = Duration(milliseconds: 750);
-  const wite = Duration(seconds: 1);
+  const wait = Duration(seconds: 1);
 
-  
+  try {
+    Future.delayed(wait, () {
+      print("I will never run");
+    }).then((value) {}).timeout(timeout);
+  } catch (_) {}
 }
