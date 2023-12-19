bde9bb7864a7112b676fcc21f810db13a8a13314
bastndev <gohit24@gmail.com>
2023-12-19 10:37:48 -0500

    :arrow_up: Update `new`

--- 10-FUTURE/chronometer.dart
+++ 10-FUTURE/chronometer.dart
@@ -4,10 +4,23 @@ void main() async {
   while (seconds < 60) {
     await Future.delayed(const Duration(milliseconds: 900));
     seconds++;
-    print(seconds);
+    print(seconds.toTimer());
   }
 }
 
-extension TimerExtension on int{
+extension TimerExtension on int {
+  String toTimer() {
+    int hours = (this ~/ 3600).toInt();
+    String hoursString = hours < 10 ? '0$hours' : '$hours';
 
+    int remainingSeconds = this % 3600;
+    int minutes = (remainingSeconds ~/ 60).toInt();
+    String minutesString = minutes < 10 ? '0$minutes' : '$minutes';
+
+    remainingSeconds = remainingSeconds % 60;
+    String secondsString =
+        remainingSeconds < 10 ? '0$remainingSeconds' : '$remainingSeconds';
+
+    return '$hoursString:$minutesString:$secondsString';
+  }
 }
