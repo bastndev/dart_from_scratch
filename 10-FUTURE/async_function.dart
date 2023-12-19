4cd741f4cbbb673160d98d10de44128d88ceb8f2
bastndev <gohit24@gmail.com>
2023-12-19 09:36:50 -0500

    :arrow_up: Update `new`

---
+++ 10-FUTURE/async_function.dart
@@ -0,0 +1,13 @@
+class Product {
+  String name;
+  num product;
+
+  Product(this.name, this.product);
+
+  @override
+  String toString() => 'Product(name: $name, product: $product)';
+}
+
+void main() { 
+  
+}
