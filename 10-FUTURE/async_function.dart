38ab0f56e962c21ae9b3bac1574693e2def043f6
bastndev <gohit24@gmail.com>
2023-12-19 09:45:52 -0500

    :arrow_up: Update `new`

--- 10-FUTURE/async_function.dart
+++ 10-FUTURE/async_function.dart
@@ -5,9 +5,19 @@ class Product {
   Product(this.name, this.product);
 
   @override
-  String toString() => 'Product(name: $name, product: $product)';
+  String toString() => 'Product(name: $name, Price: $product)';
 }
 
-void main() { 
-  
+void main() {
+  print("1: Initializing the program...");
+
+  getProduct().then(print);
+
+  print("3: Finished the program");
+}
+
+Future<Product> getProduct() {
+  return Future.delayed(const Duration(milliseconds: 1500), () {
+    return Product("Cadillac", 1200);
+  });
 }
