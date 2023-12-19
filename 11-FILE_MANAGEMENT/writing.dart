50a12d47dcea87d7ce43ac4bda03f56458d9206d
bastndev <gohit24@gmail.com>
2023-12-19 15:30:17 -0500

    :arrow_up: Update `new`

--- 11-FILE_MANAGEMENT/writing.dart
+++ 11-FILE_MANAGEMENT/writing.dart
@@ -1,5 +1,13 @@
 import 'dart:io';
 
-void main() { 
-  
+void main() async {
+  const filesFolder = 'zz/';
+  const testPath = '${filesFolder}test.txt';
+
+  final file = File(testPath);
+
+  int interaction = 10;
+  for (int i = 0; i < interaction; i++) {
+    file.writeAsString("Writing text in test ${i + 1} \n");
+  }
 }
