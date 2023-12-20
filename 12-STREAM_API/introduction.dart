8adb5a18f3fc58d9757cbea6686ef1c6f7b40936
bastndev <gohit24@gmail.com>
2023-12-20 10:21:10 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/introduction.dart
+++ 12-STREAM_API/introduction.dart
@@ -1,7 +1,20 @@
+//--- --- -FIXME: (Subscribe) pause, play cancel
 import 'dart:io';
 
-void main() { 
+void main() async {
   final file = File('files/long.txt');
 
-  
+  Stream<List<int>> readStream = file.openRead();
+
+  int events = 0;
+
+  await for (final data in readStream) {
+    events++;
+  }
+
+  final kb = file.lengthSync() / 1024;
+  final averageKb = kb / events;
+
+  print("Events: $events");
+  print("Average size  of each event: $averageKb");
 }
