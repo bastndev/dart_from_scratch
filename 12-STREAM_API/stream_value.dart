739c2ce71eadb41055de1aaca768cb713c1a1d96
bastndev <gohit24@gmail.com>
2023-12-20 15:52:42 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/stream_value.dart
+++ 12-STREAM_API/stream_value.dart
@@ -25,3 +25,34 @@
   // stream.listen((event) {});
 }
  */
+
+/* void main() async {
+  // final stream = Stream<int>.value(5);
+
+  final stream = Stream.fromIterable([1, 2, 3, 4, 5, 6]);
+  // bool containsTen = await stream.contains(10);
+  // print(containsTen);
+  stream.where((event) => event > 2).listen(print);
+  stream.listen((event) {});
+
+  // stream.listen((event) {});
+} */
+
+import 'dart:async';
+
+void main() async {
+  final stream = Stream<int>.value(5);
+
+  // final stream = Stream.fromIterable([1, 2, 3, 4, 5, 6]);
+  // bool containsTen = await stream.contains(10);
+  // print(containsTen);
+  // stream.where((event) => event > 2).listen(print);
+  // stream.listen((event) {});
+
+  // stream.listen((event) {});
+
+  final newStream = stream.where((event) => event > 4);
+  final controller = StreamController<int>();
+  controller.addStream(newStream);
+  controller.stream.listen(print);
+}
