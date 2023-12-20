92de1183b4951069e970dcd1abb252b0b44651d3
bastndev <gohit24@gmail.com>
2023-12-20 18:09:50 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/where_distinct.dart
+++ 12-STREAM_API/where_distinct.dart
@@ -1,3 +1,11 @@
-void main() { 
-  
+/* void main() {
+  final stream = Stream<int>.fromIterable([1, 2, 3, 3, 3, 4, 5, 6]);
+  final filterStream = stream.where((event) => event > 3);
+  filterStream.listen(print);
+}
+ */
+void main() {
+  final stream = Stream<int>.fromIterable([1, 2, 3, 3, 3, 4, 5, 6]);
+
+  stream.distinct((a, b) => a == b).listen(print);
 }
