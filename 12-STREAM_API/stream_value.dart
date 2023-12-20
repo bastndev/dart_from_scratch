344e9ac7e5af17b23ea737658d5ecd060afe8752
bastndev <gohit24@gmail.com>
2023-12-20 15:35:43 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/stream_value.dart
+++ 12-STREAM_API/stream_value.dart
@@ -1,3 +1,16 @@
-void main() { 
-  
+// -- -- -FIXME: Stream con value is mono subscription recommend(1)
+/* void main() {
+  final stream = Stream<int>.value(5);
+
+  stream.listen((event) {});
+  stream.listen((event) {});
+}
+ */
+
+
+// --- ---  Multi subscription
+void main() {
+  final stream = Stream.fromIterable([1, 2, 3, 4, 5, 6]);
+  stream.listen((event) {});
+  stream.listen((event) {});
 }
