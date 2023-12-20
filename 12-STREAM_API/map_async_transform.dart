4fbf0a79a77008625c8dc14083ccfe944dc2a258
bastndev <gohit24@gmail.com>
2023-12-20 18:31:49 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/map_async_transform.dart
+++ 12-STREAM_API/map_async_transform.dart
@@ -1,3 +1,13 @@
+//Transform the int to string
+import 'dart:async';
+
 void main() {
   final stream = Stream.fromIterable([10, 11, 12, 3, 4, 5, 6, 10]);
+  Stream<String> transformStream =
+      stream.transform(StreamTransformer.fromHandlers(
+    handleData: (data, sink) {
+      sink.add('transforming: $data');
+    },
+  ));
+  transformStream.listen(print);
 }
