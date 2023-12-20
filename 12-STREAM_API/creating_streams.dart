6f24762721683c03a4d1ac5978056a80006f082d
bastndev <gohit24@gmail.com>
2023-12-20 10:36:02 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/creating_streams.dart
+++ 12-STREAM_API/creating_streams.dart
@@ -9,3 +9,14 @@
 }
  */
 
+void main() {
+  Stream<int> stream = Stream.error("This is a message to error");
+
+  stream.listen((event) {
+    print(event);
+  }, onDone: () {
+    print("Finished Stream");
+  }, onError: (err){
+    print("Hello $err");
+  });
+}
