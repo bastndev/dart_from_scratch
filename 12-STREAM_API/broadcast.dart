10f938e1e51756822a3072341940e842adfd7fce
bastndev <gohit24@gmail.com>
2023-12-20 11:40:17 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/broadcast.dart
+++ 12-STREAM_API/broadcast.dart
@@ -1,7 +1,7 @@
 // --- --- -FIXME: Multi Subscription
 import 'dart:async';
 
-void main() {
+/* void main() {
   final controller = StreamController<int>.broadcast();
   Stream<int> broadcastStream = Stream.value(5).asBroadcastStream();
 
@@ -12,3 +12,21 @@ void main() {
     print("Sub2: $event");
   });
 }
+ */
+
+
+
+/* void main() {
+  final controller = StreamController<int>.broadcast(sync: true);
+  Stream<int> broadcastStream = controller.stream;
+
+  final sub1 = broadcastStream.listen((event) {
+    print("Sub1: $event");
+  });
+  final sub2 = broadcastStream.listen((event) {
+    print("Sub2: $event");
+  });
+
+  controller.add(5);
+  sub1.pause();
+} */
