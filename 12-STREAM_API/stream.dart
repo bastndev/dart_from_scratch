8c2a99bfd23a161b71935cd33c8928465638e73a
bastndev <gohit24@gmail.com>
2023-12-20 17:40:53 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/stream.dart
+++ 12-STREAM_API/stream.dart
@@ -1,12 +1,19 @@
+import 'dart:async';
+
 void main() { 
   //StreamController
-
+  StreamController<int> controller = StreamController<int>(); //create controller
   //Stream
-
+  Stream stream = controller.stream; //chanel for share data
 
   // Subscription
+  StreamSubscription subscription = stream.listen((value) { //reception value
+    print("This is the value: $value");
+  });
 
   //add a value
-
+  controller.add(10);
+  controller.add(20);
+  controller.add(30);
   //EventSink
 }
