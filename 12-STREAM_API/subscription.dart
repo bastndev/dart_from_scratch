a28c649c07aaf56e383e366c0f8264af024700e9
bastndev <gohit24@gmail.com>
2023-12-20 10:46:01 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/subscription.dart
+++ 12-STREAM_API/subscription.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-void main() {
+/* void main() {
   StreamSubscription<int> subscription;
   Stream<int> stream = Stream.value(2);
 
@@ -8,4 +8,16 @@ void main() {
 
   subscription.pause();
   subscription.resume();
+  subscription.cancel();
+}
+ */
+
+void main() {
+  StreamSubscription<int> subscription;
+  Stream<int> stream = Stream.value(2);
+
+  subscription = stream.listen(print);
+
+  subscription.pause(Future.delayed(const Duration(milliseconds: 1000)));
+  // subscription.cancel();
 }
