8f5c762dfc10338f45d3904335901497f4dc320b
bastndev <gohit24@gmail.com>
2023-12-20 18:01:55 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/stream_async.dart
+++ 12-STREAM_API/stream_async.dart
@@ -1,3 +1,14 @@
-void main() { 
-  
+void main() async {
+  await for (int i in oneNumberEverySecond()) {
+    print(i);
+  }
+}
+// async*
+// yield
+
+Stream<int> oneNumberEverySecond() async* {
+  for (int i = 0; i < 5; i++) {
+    await Future.delayed(const Duration(milliseconds: 1000));
+    yield i;
+  }
 }
