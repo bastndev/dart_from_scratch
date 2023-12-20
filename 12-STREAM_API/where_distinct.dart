6ef2022c2be02b39d501cf30245f6e5d2c73fad1
bastndev <gohit24@gmail.com>
2023-12-20 18:20:56 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/where_distinct.dart
+++ 12-STREAM_API/where_distinct.dart
@@ -4,8 +4,36 @@
   filterStream.listen(print);
 }
  */
-void main() {
+/* void main() {
   final stream = Stream<int>.fromIterable([1, 2, 3, 3, 3, 4, 5, 6]);
 
   stream.distinct((a, b) => a == b).listen(print);
+} */
+
+import 'dart:async';
+
+void main() {
+  final stream = Stream<int>.fromIterable([1, 2, 3, 3, 3, 4, 5, 6]);
+
+  final connected = 'CONNECTED :)';
+  // stream.distinct((a, b) => a == b).listen(print);
+  final controller = StreamController<String>();
+  controller.stream.distinct((a, b) {
+    return a == b;
+  }).listen((event) {
+    print("Status connection: $event");
+    if (event == connected) {
+      print('Connected');
+    }
+  });
+
+  for (int i = 0; i < 100; i++) {
+    if (i == 0) {
+      controller.add('PREPARING...');
+    } else if (i < 99) {
+      controller.add('WAITING...');
+    } else {
+      controller.add(connected);
+    }
+  }
 }
