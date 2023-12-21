46d2fa3e6224742a8958931586400ff0e788cdc3
bastndev <gohit24@gmail.com>
2023-12-21 09:07:23 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/tolist_toset.dart
+++ 12-STREAM_API/tolist_toset.dart
@@ -1,6 +1,13 @@
-void main() async { 
-  final stream = Stream.fromIterable ([1,2,3,3,4,5,6]);
+/* void main() async { 
+  final stream = Stream.fromIterable ([1,2,3,3,4,5,6,6]);
 
   final list = await stream.toList();
   print(list);
+} */
+
+void main() async { 
+  final stream = Stream.fromIterable ([1,2,3,3,4,5,6,6]);
+
+  final sett = await stream.toSet();
+  print(sett);
 }
