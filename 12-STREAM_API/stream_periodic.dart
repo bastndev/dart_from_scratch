ae29d69718420796f7e31f368f62cc109e6a4cdd
bastndev <gohit24@gmail.com>
2023-12-21 10:16:55 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/stream_periodic.dart
+++ 12-STREAM_API/stream_periodic.dart
@@ -21,3 +21,18 @@ import 'dart:async';
   });
 }
  */
+
+void main() {
+  StreamSubscription<int>? subs;
+  final stream = Stream.periodic(const Duration(seconds: 1), (event) {
+    if (event == 5) {
+      subs?.pause(Future.delayed(const Duration(seconds: 2)));
+    }else if (event == 15) {
+      subs?.cancel();
+    }
+    return event;
+  });
+  subs = stream.listen((event) {
+    print(event);
+  });
+}
