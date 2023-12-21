973310fe884456085a58f187f76a398760d01710
bastndev <gohit24@gmail.com>
2023-12-21 07:43:08 -0500

    :arrow_up: Update `new`

---
+++ 12-STREAM_API/fold_stream.dart
@@ -0,0 +1,6 @@
+/* void main() async {
+  final stream = Stream.fromIterable([1, 2, 3, 4, 5, 6]);
+
+  final sum = await stream.fold(0, (previous, element) => previous + element);
+  print("The sum of elements is: $sum");
+} */
