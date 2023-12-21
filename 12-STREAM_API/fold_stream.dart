66905458e2301855fc02bd286c2643cd10d35414
bastndev <gohit24@gmail.com>
2023-12-21 07:52:50 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/fold_stream.dart
+++ 12-STREAM_API/fold_stream.dart
@@ -5,11 +5,21 @@
   print("The sum of elements is: $sum");
 } */
 
-void main() async {
+/* void main() async {
   final stream = Stream.fromIterable([1, 2, 3, 4, 5, 6]);
 
   final length = await stream.length;
   final average = await stream.fold(
       0.0, (previous, element) => previous + (element / length));
   print("The division between all numbers is: $average");
+} */
+
+// --- -More easy
+void main() async {
+  final stream = Stream.fromIterable([1, 2, 3, 4, 5, 6]);
+
+  final sum = await stream.fold(0, (previous, element) => previous + element);
+  final length = await stream.length;
+  
+  print("The division between all numbers is: ${sum / length}");
 }
