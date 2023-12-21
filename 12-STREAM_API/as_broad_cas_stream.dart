4d51e054fe7f615579cfe85e0888315210a23795
bastndev <gohit24@gmail.com>
2023-12-21 07:31:44 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/as_broad_cas_stream.dart
+++ 12-STREAM_API/as_broad_cas_stream.dart
@@ -1,3 +1,8 @@
-void main() { 
-  
+// --- --- -FIXME: asBroadcastStream transform one stream mono subscription to stream multi Subscription
+void main() {
+  Stream<int> stream = Stream.value(5);
+  Stream<int> broadcastStream = stream.asBroadcastStream();
+
+  broadcastStream.listen((event) { });
+  broadcastStream.listen((event) { });
 }
