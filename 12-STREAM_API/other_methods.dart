11e6333518a8487fda8dcc70580e991dbe653793
bastndev <gohit24@gmail.com>
2023-12-21 09:33:56 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/other_methods.dart
+++ 12-STREAM_API/other_methods.dart
@@ -1,8 +1,15 @@
-void main() async {
+/* void main() async {
   final stream = Stream.fromIterable([6, 5, 4, 3, 2, 1]);
 
   final containsFive = await stream.contains(5);
   print(containsFive);
   final containsTen =  await stream.contains(10);
   print(containsTen);
+} */
+
+void main() async {
+  final stream = Stream.fromIterable([6, 5, 4, 3, 2, 1]);
+
+  final containsGreaterThanThree = await stream.any((element) => element > 3);
+  print(containsGreaterThanThree);
 }
