d554b1388e8dae85d9e5fd2c633d1cb5943d53be
bastndev <gohit24@gmail.com>
2023-12-21 08:12:09 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/take.dart
+++ 12-STREAM_API/take.dart
@@ -1,7 +1,23 @@
-void main() { 
+/* void main() { 
   final stream = Stream.fromIterable([6,5,4,3,2,1,]);
 
   stream.take(1).listen((event) {
+    print("The first number i: $event.");
+  });
+} */
+
+/* void main() { 
+  final stream = Stream.fromIterable([6,5,4,3,2,1,]);
+
+  stream.take(3).listen((event) {
+    print("The first number i: $event.");
+  });
+} */
+
+void main() { 
+  final stream = Stream.fromIterable([6,5,4,3,2,1,]);
+
+  stream.take(30).listen((event) {
     print("The first number i: $event.");
   });
 }
