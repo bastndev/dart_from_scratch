6a826cb829ddfdb40ae3e03cc0a024521ca79e18
bastndev <gohit24@gmail.com>
2023-12-21 10:58:52 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/generators.dart
+++ 12-STREAM_API/generators.dart
@@ -1,4 +1,4 @@
-void main() {
+/* void main() {
   print("Start program");
 
   getStream().listen(print);
@@ -9,3 +9,22 @@ void main() {
 Stream<int> getStream() {
   return Stream.fromIterable([1, 2, 3, 4, 5]);
 }
+ */
+
+/* void main() async{
+  print("Start program");
+
+  final list = await getGeneration().toList();
+  print(list);
+
+  print("Finished program");
+}
+
+Stream<int> getGeneration() async* {
+  final list = [5, 6, 9];
+  for (int n in list) {
+    yield n;
+    yield n + 1;
+    yield n + 2;
+  }
+} */
