89f766718a0ebc8292c8285ffbd878921f5877f2
bastndev <gohit24@gmail.com>
2023-12-21 09:58:04 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/await_for.dart
+++ 12-STREAM_API/await_for.dart
@@ -1,8 +1,16 @@
-void main() async {
+/* void main() async {
   final stream = Stream.fromIterable([1, 2, 3, 3, 4, 5, 6, 6]);
 
   await for (final data in stream) {
     print(data);
   }
+  print("I run after (await for )");
+}
+ */
+
+void main() async {
+  final stream = Stream.fromIterable([1, 2, 3, 3, 4, 5, 6, 6]);
+
+  stream.listen(print);
   print("I run after (await for )");
 }
