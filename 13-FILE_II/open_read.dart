28cb5e551f14f33f644070cd67739a669b4dbf54
bastndev <gohit24@gmail.com>
2023-12-21 15:20:21 -0500

    :arrow_up: Update `new`

--- 13-FILE_II/open_read.dart
+++ 13-FILE_II/open_read.dart
@@ -11,10 +11,21 @@ import 'dart:io';
 }
  */
 
-void main() {
+/* void main() {
   final file = File('files/books.csv');
 
   final readStream = file.openRead();
+
+  readStream.listen((event) {
+    final text = String.fromCharCodes(event);
+    print(text);
+  });
+} */
+
+void main() {
+  final file = File('files/books.csv');
+
+  final readStream = file.openRead(0,2);
 
   readStream.listen((event) {
     final text = String.fromCharCodes(event);
