39930509e186c1163d9f4d5a65c49dfc1ce54de6
bastndev <gohit24@gmail.com>
2023-12-21 09:59:59 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/await_for.dart
+++ 12-STREAM_API/await_for.dart
@@ -8,9 +8,19 @@
 }
  */
 
-void main() async {
+/* void main() async {
   final stream = Stream.fromIterable([1, 2, 3, 3, 4, 5, 6, 6]);
 
   stream.listen(print);
+  print("I run after (await for )");
+} */
+
+// --will not be read until the loop is finished.
+void main() async {
+  final stream = Stream.fromIterable([1, 2, 3, 3, 4, 5, 6, 6]);
+
+  await for (final data in stream) {
+    print(data);
+  }
   print("I run after (await for )");
 }
