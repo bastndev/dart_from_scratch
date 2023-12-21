8a32fef727d67c6e4f067bb9bb3501e4e5262ee7
bastndev <gohit24@gmail.com>
2023-12-21 10:50:10 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/generators.dart
+++ 12-STREAM_API/generators.dart
@@ -1,3 +1,11 @@
-void main() async { 
-  
+void main() {
+  print("Start program");
+
+  getStream().listen(print);
+
+  print("Finished program");
+}
+
+Stream<int> getStream() {
+  return Stream.fromIterable([1, 2, 3, 4, 5]);
 }
