62640705ffea365d30f8f689fa0158db07cda866
bastndev <gohit24@gmail.com>
2023-12-21 08:13:35 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/take.dart
+++ 12-STREAM_API/take.dart
@@ -23,10 +23,18 @@
 } */
 
 // --- 
-void main() { 
+/* void main() { 
   final stream = Stream.fromIterable([6,5,4,3,2,1,]);
 
   stream.skip(3).listen((event) {
+    print("The first number i: $event.");
+  });
+} */
+
+void main() { 
+  final stream = Stream.fromIterable([6,5,4,3,2,1,]);
+
+  stream.skip(30).listen((event) {
     print("The first number i: $event.");
   });
 }
