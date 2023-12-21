e80fb86363cae58e5e8bb6fe5938608f9b758c15
bastndev <gohit24@gmail.com>
2023-12-21 11:14:58 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/generators.dart
+++ 12-STREAM_API/generators.dart
@@ -28,3 +28,59 @@ Stream<int> getGeneration() async* {
     yield n + 2;
   }
 } */
+
+/* void main() {
+  print("Start program");
+
+  final list =  getGeneration().toList();
+  print(list);
+
+  print("Finished program");
+}
+
+Iterable<int> getGeneration() sync* {
+  final list = [5, 6, 9];
+  for(int n in list) {
+    yield n;
+    yield n+1;
+    yield n+2;
+  }
+}
+ */
+/* void main() {
+  print("Start program");
+
+  final list = getGeneration().toList();
+  print(list);
+
+  print("Finished program");
+}
+
+Iterable<int> getGeneration() sync* {
+  final list = [5, 6, 7];
+  for (int n in list) {
+    yield n;
+    yield n+1;
+    yield n+2;
+  }
+} */
+
+import 'dart:math';
+
+void main() {
+  print("Start program");
+
+  getNumbers().listen(print);
+
+
+  print("Finished program");
+}
+//- function getNumbers()
+Stream<int> getNumbers() async* {
+  Stream stream = Stream.fromIterable([5, 6, 9]);
+  await for (int data in stream) {
+    yield pow(data, 2).toInt();
+    await Future.delayed(const Duration(seconds: 1));
+    yield data;
+  }
+}
