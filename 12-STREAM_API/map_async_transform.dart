68c010459115c50ceeb3daa5e1c88127fe62ffdd
bastndev <gohit24@gmail.com>
2023-12-20 19:02:13 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/map_async_transform.dart
+++ 12-STREAM_API/map_async_transform.dart
@@ -14,8 +14,34 @@ import 'dart:async';
  */
 
 // more easy
-void main() {
+/* void main() {
   final stream = Stream.fromIterable([10, 11, 12, 3, 4, 5, 6, 10]);
 
   stream.map((event) => 'transforming: $event').listen(print);
+} */
+
+//async
+/* void main() async {
+  final stream = Stream.fromIterable([10, 11, 12, 3, 4, 5, 6, 10]);
+
+  Stream<String> asyncMapStream = stream.asyncMap((event) {
+    return Future.delayed(const Duration(milliseconds: 1000), () {
+      return 'transforming: $event';
+    });
+  });
+
+  await for (var value in asyncMapStream) {
+    print(value);
+  }
+} */
+
+//--FIXME: recommended async
+void main() async {
+  final stream = Stream.fromIterable([10, 11, 12, 3, 4, 5, 6, 10]);
+
+  Stream<String> asyncMapStream = stream.asyncMap(
+      (event) => Future.delayed(const Duration(milliseconds: 1000), () {
+            return 'transforming: $event';
+          }));
+  asyncMapStream.listen(print);
 }
