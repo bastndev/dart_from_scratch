053cb4ac23fead2869fd1c13895a9bba1998782c
bastndev <gohit24@gmail.com>
2023-12-21 08:06:31 -0500

    :arrow_up: Update `new`

--- 12-STREAM_API/pipe.dart
+++ 12-STREAM_API/pipe.dart
@@ -1,9 +1,26 @@
 import 'dart:async';
 
-void main() async {
+/* void main() async {
   final stream = Stream.fromIterable([1, 2, 3, 4, 5, 6]);
   final controller = StreamController<int>();
 
   stream.pipe(controller);
   controller.stream.listen(print);
+} */
+
+
+/* void main() async {
+  final stream = Stream.fromIterable([1, 2, 3, 4, 5, 6]);
+  final controller = StreamController<int>();
+
+  await stream.pipe(controller);
+  controller.stream.listen(print);
+} */
+
+void main() async {
+  final stream = Stream.fromIterable([1, 2, 3, 4, 5, 6]);
+  final controller = StreamController<int>();
+
+  controller.stream.listen(print);
+  await stream.pipe(controller);
 }
