a3935b394d8e240f969d25061c980098f9198b8f
bastndev <gohit24@gmail.com>
2023-12-22 10:45:25 -0500

    :arrow_up: Update `new`

---
+++ 14-SOCKETS/tcp_client.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() async {
+  Socket socket = await Socket.connect('127.0.0.1', 4040);
+
+  socket.listen((event) {
+    print('Server response');
+    final response = String.fromCharCodes(event);
+    print(response);
+  });
+
+  stdout.write('Write a message: ');
+  final smg = stdin.readLineSync();
+  socket.write(smg);
+}
