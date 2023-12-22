869c07e083b5ebde3a548dd11b95d2febe9433e9
bastndev <gohit24@gmail.com>
2023-12-22 09:07:38 -0500

    :arrow_up: Update `new`

--- 14-SOCKETS/udp_a.dart
+++ 14-SOCKETS/udp_a.dart
@@ -1,5 +1,22 @@
 import 'dart:io';
 
-void main() async { 
-  
+void main() async {
+  final localhost = '120.0.0.1';
+  final port = 16000;
+  final receiverPort = 16001;
+
+  final udpSocket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, port);
+
+  udpSocket.listen((event) {
+    final datagram = udpSocket.receive();
+    if (datagram != null) {
+      final bytes = datagram.data;
+      final decoded = String.fromCharCodes(bytes);
+      print("Message received: $decoded");
+      stdout.write('Write the message: ');
+      final message = stdin.readLineSync() ?? '';
+      udpSocket.send(
+          message.codeUnits, InternetAddress(localhost), receiverPort);
+    }
+  });
 }
