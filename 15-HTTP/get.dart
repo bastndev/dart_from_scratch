97f83c0ba5f9475c2207fa3eb3c3717232884515
bastndev <gohit24@gmail.com>
2023-12-22 15:52:29 -0500

    :arrow_up: Update `new`

--- 15-HTTP/get.dart
+++ 15-HTTP/get.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
-import 'package:http/http.dart'
-    as http; //as htp indica un espacio de nombre para la biblioteca http
+//as http indica un espacio de nombre para la biblioteca http
+import 'package:http/http.dart' as http;
 
 /* void main() async {
   final url = Uri.parse('https://reqres.in/api/users/2');
@@ -25,6 +25,18 @@ import 'package:http/http.dart'
  */
 
 //Email
+/* void main() async {
+  final url = Uri.parse('https://reqres.in/api/users/2');
+  http.Response response = await http.get(url);
+
+  //mapearlo
+  final data = jsonDecode(response.body);
+  print(data['data']['email']);
+  print("Type response: ${data.runtimeType}");
+}
+ */
+
+// -FIXME: GETTING BETTER
 void main() async {
   final url = Uri.parse('https://reqres.in/api/users/2');
   http.Response response = await http.get(url);
@@ -33,4 +45,21 @@ void main() async {
   final data = jsonDecode(response.body);
   print(data['data']['email']);
   print("Type response: ${data.runtimeType}");
+}
+
+// builder - Constructor
+class User {
+  final int id;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String avatar;
+
+  User({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
+  });
 }
