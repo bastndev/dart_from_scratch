678356e9b7b33e7125072464193a5f47c36a2850
bastndev <gohit24@gmail.com>
2023-12-22 16:31:04 -0500

    :arrow_up: Update `new`

---
+++ 15-HTTP/quicktype.dart
@@ -0,0 +1,97 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+// -FIXME: GETTING BETTER
+void main() async {
+  final url = Uri.parse('https://reqres.in/api/users/2');
+  http.Response response = await http.get(url);
+
+  //mapearlo
+  final user = userFromJson(response.body);
+
+  print(user.data.email);
+  print("Type response: ${user.runtimeType}");
+}
+
+// To parse this JSON data, do
+//
+//     final user = userFromJson(jsonString);
+
+// To parse this JSON data, do
+//
+//     final user = userFromJson(jsonString);
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+String userToJson(User data) => json.encode(data.toJson());
+
+class User {
+  final Data data;
+  final Support support;
+
+  User({
+    required this.data,
+    required this.support,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        data: Data.fromJson(json["data"]),
+        support: Support.fromJson(json["support"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "data": data.toJson(),
+        "support": support.toJson(),
+      };
+}
+
+class Data {
+  final int id;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String avatar;
+
+  Data({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
+  });
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        id: json["id"],
+        email: json["email"],
+        firstName: json["first_name"],
+        lastName: json["last_name"],
+        avatar: json["avatar"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "email": email,
+        "first_name": firstName,
+        "last_name": lastName,
+        "avatar": avatar,
+      };
+}
+
+class Support {
+  final String url;
+  final String text;
+
+  Support({
+    required this.url,
+    required this.text,
+  });
+
+  factory Support.fromJson(Map<String, dynamic> json) => Support(
+        url: json["url"],
+        text: json["text"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "url": url,
+        "text": text,
+      };
+}
