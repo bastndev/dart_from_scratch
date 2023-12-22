9e45d537f820e6188450a6be71a6ddc1a53ab381
bastndev <gohit24@gmail.com>
2023-12-22 11:29:20 -0500

    :arrow_up: Update `new`

--- 14-SOCKETS/tcp2_client.dart
+++ 14-SOCKETS/tcp2_client.dart
@@ -2,12 +2,12 @@ import 'dart:io';
 
 void main() async {
   Socket socket = await Socket.connect('127.0.0.1', 4040);
-
+  final file = File('files/long.txt');
   socket.listen((event) {
-    print('Server response');
+    print('Server response!');
     final response = String.fromCharCodes(event);
     print(response);
   });
 
-  
+  socket.addStream(file.openRead());
 }
