8f0c121ccf5ac675bee69867e9cb3d94a772b997
bastndev <gohit24@gmail.com>
2023-12-22 09:38:18 -0500

    :arrow_up: Update `new`

--- 14-SOCKETS/udp_a.dart
+++ 14-SOCKETS/udp_a.dart
@@ -20,3 +20,25 @@ void main() async {
     }
   });
 }
+
+class DatagramLogger {
+  static const path = '14-SOCKETS/log';
+  static File? _file;
+
+  DatagramLogger._internal() {
+    _file = File(path);
+  }
+
+  static DatagramLogger? _logger;
+  static DatagramLogger get instance => _logger ??= DatagramLogger._internal();
+
+  log(Datagram datagram) {
+    final date = DateTime.timestamp().toIso8601String();
+    final message = String.fromCharCodes(datagram.data);
+    final address = datagram.address.address;
+    final port = datagram.port;
+
+    final text = '{$date; $message; $address; $port}\n';
+    _file?.writeAsStringSync(text, mode: FileMode.append);
+  }
+}
