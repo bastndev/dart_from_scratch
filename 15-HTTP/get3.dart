d064184df179b8eadd281a825f375ad898825d2b
bastndev <gohit24@gmail.com>
2023-12-22 16:54:36 -0500

    :arrow_up: Update `new`

--- 15-HTTP/get3.dart
+++ 15-HTTP/get3.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 // -FIXME: GETTING BETTER
@@ -6,9 +7,9 @@ void main() async {
   final url = Uri.parse('https://reqres.in/api/users/2');
   http.Response response = await http.get(url);
 
-  final user = userFromJson(response.body);
+  final user = User.fromJson(response.body);
 
-  print(user.data.email);
+  print(user.email);
   print("Type response: ${user.runtimeType}");
 }
 
@@ -20,4 +21,81 @@ class User {
   final String lastName;
   final String avatar;
 
+  const User({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
+  });
+
+  User copyWith({
+    int? id,
+    String? email,
+    String? firstName,
+    String? lastName,
+    String? avatar,
+  }) {
+    return User(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      avatar: avatar ?? this.avatar,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'email': email,
+      'firstName': firstName,
+      'lastName': lastName,
+      'avatar': avatar,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id']?.toInt() ?? 0,
+      email: map['email'] ?? '',
+      firstName: map['first_name'] ?? '',
+      lastName: map['last_name'] ?? '',
+      avatar: map['avatar'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) {
+    final map = json.decode(source);
+    final data = map['data'];
+    return User.fromMap(data as Map<String, dynamic>);
+  }
+
+  @override
+  String toString() {
+    return 'User(id: $id, email: $email, firstName: $firstName, lastName: $lastName, avatar: $avatar)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is User &&
+        other.id == id &&
+        other.email == email &&
+        other.firstName == firstName &&
+        other.lastName == lastName &&
+        other.avatar == avatar;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        email.hashCode ^
+        firstName.hashCode ^
+        lastName.hashCode ^
+        avatar.hashCode;
+  }
 }
