6c54a54e8d48770d0a96cc041b36414c0fdf6dc8
bastndev <gohit24@gmail.com>
2023-12-22 22:23:01 -0500

    :arrow_up: Update `new`

--- 15-HTTP/api_auth.dart
+++ 15-HTTP/api_auth.dart
@@ -12,7 +12,7 @@ import 'package:http/http.dart' as http;
 /* void main() async {
   final apiKey = 'fca_live_kfWL6teqkGFUgZTeRrr0fj1oEbMeGWnWoPSH8RWb';
   final url = Uri.parse(
-      'https://api.freecurrencyapi.com/v1/currencies?apikey=$apiKey&currencies=USD,EUR');
+      'https://api.freecurrencyapi.com/v1/currencies?apikey=&currencies=USD,EUR');
   final headers = {
     'Authorization': 'token',
     'Content-Type': 'application/json'
@@ -21,13 +21,12 @@ import 'package:http/http.dart' as http;
 
   print(response.statusCode);
   print(response.body);
-}
- */
+} */
 
 void main() async {
   final apiKey = 'fca_live_kfWL6teqkGFUgZTeRrr0fj1oEbMeGWnWoPSH8RWb';
   final url = Uri.parse(
-      'https://api.freecurrencyapi.com/v1/currencies?apikey=&currencies=USD,EUR');
+      'https://api.freecurrencyapi.com/v1/currencies?apikey=$apiKey&currencies=USD,EUR');
   final headers = {
     'Authorization': 'token',
     'Content-Type': 'application/json'
