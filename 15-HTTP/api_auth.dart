3c3484326960d56b94630ea927e0b203b023e84d
bastndev <gohit24@gmail.com>
2023-12-22 22:14:23 -0500

    :arrow_up: Update `new`

--- 15-HTTP/api_auth.dart
+++ 15-HTTP/api_auth.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 void main() async {
   final apiKey = 'fca_live_kfWL6teqkGFUgZTeRrr0fj1oEbMeGWnWoPSH8RWb';
-  final url = Uri.parse('https://api.freecurrencyapi.com/v1/currencies?apikey=$apiKey');
+  final url = Uri.parse('https://api.freecurrencyapi.com/v1/currencies?apikey=$apiKey&currencies=USD,EUR');
   final response = await http.get(url);
 
   print(response.statusCode);
