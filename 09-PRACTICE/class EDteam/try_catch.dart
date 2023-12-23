99d1f745469abad0d6c16e31d3da087e71644d59
bastndev <gohit24@gmail.com>
2023-12-23 09:20:17 -0500

    :arrow_up: Update `new`

--- 09-PRACTICE/class EDteam/try_catch.dart
+++ 09-PRACTICE/class EDteam/try_catch.dart
@@ -1,3 +1,4 @@
+//-FIXME: // Utilizando try-catch para manejar posibles errores de manera controlada.
 //-TODO: with error
 /* void main() {
   String userInput = '3,14';
@@ -12,8 +13,8 @@
   }
 } */
 
-// --- -FIXME: Okey  
-void main() {
+// --- -FIXME: Okey
+/* void main() {
   String userInput = '3.14';
 
   try {
@@ -23,5 +24,18 @@ void main() {
   } catch (e) {
     print("You have enter an invalid number");
     print(e);
+  }
+} */
+
+////////////////////////////////////////////////////////////////
+void main() {
+  // Using try-catch to handle potential errors gracefully.
+  try {
+    // Code that might cause an error
+    int result = 10 ~/ 0; // Division by zero
+    print("The result is $result");
+  } catch (e) {
+    // Handling the error
+    print("An error occurred: $e");
   }
 }
