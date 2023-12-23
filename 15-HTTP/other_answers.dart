83523ea60e6503688c38c985c6e4a72c3686eec5
bastndev <gohit24@gmail.com>
2023-12-22 22:35:12 -0500

    :arrow_up: Update `new`

--- 15-HTTP/other_answers.dart
+++ 15-HTTP/other_answers.dart
@@ -1,8 +1,16 @@
 import 'package:http/http.dart' as http;
 
-void main() async {
+/* void main() async {
   final url = Uri.parse('https://google.es');
   final response = await http.get(url);
   print(response.headers);
   print(response.body);  
 }
+ */
+
+/* void main() async {
+  final url = Uri.parse('https://images.dog.ceo/breeds/mountain-bernese/n02107683_3488.jpg');
+  final response = await http.get(url);
+  print(response.headers);
+}
+ */
