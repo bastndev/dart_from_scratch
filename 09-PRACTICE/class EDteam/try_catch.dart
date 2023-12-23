68aa0a2281ddb21ffcd87fa666a59877a3ece643
bastndev <gohit24@gmail.com>
2023-12-23 09:18:51 -0500

    :arrow_up: Update `new`

--- 09-PRACTICE/class EDteam/try_catch.dart
+++ 09-PRACTICE/class EDteam/try_catch.dart
@@ -1,5 +1,20 @@
-void main() {
+//-TODO: with error
+/* void main() {
   String userInput = '3,14';
+
+  try {
+    double number = double.parse(userInput);
+
+    print("The square of your number is: ${number * number}.");
+  } catch (e) {
+    print("You have enter an invalid number");
+    print(e);
+  }
+} */
+
+// --- -FIXME: Okey  
+void main() {
+  String userInput = '3.14';
 
   try {
     double number = double.parse(userInput);
