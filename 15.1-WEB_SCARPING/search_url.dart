6793b8eaf63aa5c1f5977879014d67fcd6a11a55
bastndev <gohit24@gmail.com>
2023-12-25 22:12:00 -0500

    :arrow_up: Update `new`

--- 15.1-WEB_SCARPING/search_url.dart
+++ 15.1-WEB_SCARPING/search_url.dart
@@ -1,6 +1,21 @@
 import 'package:http/http.dart' as http;
 
-void main() async {}
+void main() async {
+  List<String> keywords = [
+    'carne',
+    'leche',
+    'pescado',
+    'atun',
+    'tomate',
+    'legunbres',
+    'pollo',
+    'queso'
+  ];
+
+  int page = 1;
+
+  final response = await http.get(getUrl(keywords.first, page).toUri());
+}
 
 String getUrl(String keyboard, int page) {
   return 'https://es.openfoodfacts.org/cgi/search.pl?action=process&search_terms=$keyboard&sort_by=unique_scans_n&page_size=54&page=$page';
