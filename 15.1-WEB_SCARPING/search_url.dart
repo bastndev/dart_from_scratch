d8c0928c5447f66123c4283019e5b2bb8b57707c
bastndev <gohit24@gmail.com>
2023-12-25 23:23:24 -0500

    :arrow_up: Update `new`

--- 15.1-WEB_SCARPING/search_url.dart
+++ 15.1-WEB_SCARPING/search_url.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
+import 'package:html/dom.dart';
+import 'package:html/parser.dart';
 
 void main() async {
   List<String> keywords = [
@@ -13,8 +17,23 @@ void main() async {
   ];
 
   int page = 1;
+  final url = getUrl(keywords.first, page);
+  final response = await http.get(url.toUri());
+  Document doc = parse(response.body);
 
-  final response = await http.get(getUrl(keywords.first, page).toUri());
+  File('15.1-WEB_SCARPING/url_discover')
+      .writeAsStringSync('$url\n', mode: FileMode.append);
+
+  final ul = doc.getElementById('pages');
+  final children = ul!.children;
+  final element = children[children.length - 3];
+  final pages = int.parse(element.text);
+  ++page;
+  for (int i = page; i <= pages; i++) {
+    final url = getUrl(keywords.first, i);
+    File('15.1-WEB_SCARPING/url_discover')
+        .writeAsStringSync('$url\n', mode: FileMode.append);
+  }
 }
 
 String getUrl(String keyboard, int page) {
