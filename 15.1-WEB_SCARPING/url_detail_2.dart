2a38f7973fcf0d991d7a55f2b2ee45055e62a38a
bastndev <gohit24@gmail.com>
2023-12-27 11:13:30 -0500

    :arrow_up: Update `new`

--- 15.1-WEB_SCARPING/url_detail_2.dart
+++ 15.1-WEB_SCARPING/url_detail_2.dart
@@ -4,28 +4,35 @@ import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  final urls =
-      File('15.1-WEB_SCARPING/url_disco').readAsLinesSync().sublist(0, 5);
+  
+}
 
-  final baseUrl = 'https://es.openfoodfacts.com';
-  for (final url in urls) {
-    final uri = Uri.parse(url);
-    final response = await http.get(uri);
-    final doc = parse(response.body);
+class OpenFoodFactsDetail {
+  final String baseUrl = 'https://es.openfoodfacts.com';
+  final String readPath;
+  final String savePath;
 
-    final links = doc.getElementsByTagName('a');
-    for (final a in links) {
-      final attrs = a.attributes;
-      final href = attrs['href'];
+  OpenFoodFactsDetail(this.readPath, this.savePath);
 
-      if (attrs.containsKey('href') && href!.startsWith('/producto/')) {
-        File('15.1-WEB_SCARPING/products2')
-            .writeAsStringSync('$baseUrl$href\n', mode: FileMode.append);
+  Future<void> getDetailsUrl() async {
+    final urls = File(readPath).readAsLinesSync().sublist(0, 5);
+
+    final baseUrl = 'https://es.openfoodfacts.com';
+    for (final url in urls) {
+      final uri = Uri.parse(url);
+      final response = await http.get(uri);
+      final doc = parse(response.body);
+
+      final links = doc.getElementsByTagName('a');
+      for (final a in links) {
+        final attrs = a.attributes;
+        final href = attrs['href'];
+
+        if (attrs.containsKey('href') && href!.startsWith('/producto/')) {
+          File(savePath)
+              .writeAsStringSync('$baseUrl$href\n', mode: FileMode.append);
+        }
       }
     }
   }
-
-/*   final response =  await http.get(Uri.parse('https://es.openfoodfacts.org/cgi/search.pl?action=process&search_terms=carne&sort_by=unique_scans_n&page_size=54&page=1'));
-
-  File('files/test.html').writeAsStringSync(response.body); */
 }
