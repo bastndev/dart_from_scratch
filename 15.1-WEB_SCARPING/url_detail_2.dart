daba9d13735590d56fa6c820798c4503ef605294
bastndev <gohit24@gmail.com>
2023-12-27 11:21:59 -0500

    :arrow_up: Update `new`

--- 15.1-WEB_SCARPING/url_detail_2.dart
+++ 15.1-WEB_SCARPING/url_detail_2.dart
@@ -4,7 +4,9 @@ import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  
+  final detailsSearch = OpenFoodFactsDetail(
+      '15.1-WEB_SCARPING/url_disco', '15.1-WEB_SCARPING/products2');
+  detailsSearch.getDetailsUrl();
 }
 
 class OpenFoodFactsDetail {
@@ -19,20 +21,24 @@ class OpenFoodFactsDetail {
 
     final baseUrl = 'https://es.openfoodfacts.com';
     for (final url in urls) {
+      print('search products in: $url');
       final uri = Uri.parse(url);
       final response = await http.get(uri);
       final doc = parse(response.body);
 
       final links = doc.getElementsByTagName('a');
+      int count = 0;
       for (final a in links) {
         final attrs = a.attributes;
         final href = attrs['href'];
 
         if (attrs.containsKey('href') && href!.startsWith('/producto/')) {
+          ++count;
           File(savePath)
               .writeAsStringSync('$baseUrl$href\n', mode: FileMode.append);
         }
       }
+      print('Products found $count');
     }
   }
 }
