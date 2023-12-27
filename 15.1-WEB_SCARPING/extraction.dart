e6298997a422d55ac55c3bf19f332b1c4050a485
bastndev <gohit24@gmail.com>
2023-12-27 14:42:14 -0500

    :arrow_up: Update `new`

---
+++ 15.1-WEB_SCARPING/extraction.dart
@@ -0,0 +1,19 @@
+import 'package:html/parser.dart';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  final response = await http.get(Uri.parse(
+      'https://es.openfoodfacts.org/producto/8421384012014/jamon-serrano-incarlopsa'));
+
+  final doc = parse(response.body);
+  final h2s = doc.getElementsByTagName('h2');
+  String productName = '';
+
+  for (final h2 in h2s) {
+    if (h2.attributes.containsValue('food:name')) {
+      productName = h2.text;
+      break;
+    }
+  }
+  print("Product: $productName");
+}
