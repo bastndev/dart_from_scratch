d0793645c447e18f31d0729a2051f85c9c35ea69
bastndev <gohit24@gmail.com>
2023-12-28 14:52:06 -0500

    :arrow_up: Update `new`

--- 16-bbdd/select.dart
+++ 16-bbdd/select.dart
@@ -1,7 +1,10 @@
 import 'mysql_connection.dart';
 
-void main() async { 
-  handleConnection((conn) async{
-    
+void main() async {
+  handleConnection((conn) async {
+    final result = await conn.execute('SELECT * FROM users');
+    final rows = result.rows.toList();
+    final data = rows.map((e) => e.typedAssoc()).toList();
+    print(data);
   });
 }
