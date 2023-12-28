0583c1788d47aa876f3e29518ab268b5a5e3086c
bastndev <gohit24@gmail.com>
2023-12-28 14:17:52 -0500

    :arrow_up: Update `new`

--- 16-bbdd/mysql_connection.dart
+++ 16-bbdd/mysql_connection.dart
@@ -6,11 +6,27 @@ void main() async {
     port: 3306,
     userName: "root",
     password: "root",
-    databaseName: "test", // optional
+    databaseName: "test", 
   );
 
 // actually connect to database
   await conn.connect();
 
+  await conn.close();
+}
+
+//--- --- -Function  async
+Future<void> handleConnection(Function(MySQLConnection) function) async {
+  final conn = await MySQLConnection.createConnection(
+    host: "127.0.0.1",
+    port: 3306,
+    userName: "root",
+    password: "root",
+    databaseName: "test",// optional
+  );
+
+// actually connect to database
+  await conn.connect();
+  await function(conn);
   await conn.close();
 }
