676b8b0d0ce3260ea8d4f7702418101efcd3c08e
bastndev <gohit24@gmail.com>
2023-12-29 10:15:45 -0500

    :arrow_up: Update `new`

--- 17-ISOLATES/create_isolates.dart
+++ 17-ISOLATES/create_isolates.dart
@@ -15,7 +15,7 @@ import 'dart:isolate';
 }
  */
 
-void main() {
+/* void main() {
   final mainIsolate = Isolate.current;
   final mainIsolateID = Service.getIsolateId(mainIsolate);
   print(mainIsolateID);
@@ -25,4 +25,25 @@ void main() {
   Isolate.spawn((message) {
     print("Message inside isolate: $message");
   }, list);
+} */
+
+void main() {
+  final mainIsolate = Isolate.current;
+  final mainIsolateID = Service.getIsolateId(mainIsolate);
+  print(mainIsolateID);
+
+  final user = User(name: 'Gohit', email: 'pablito@gmail.com');
+
+  Isolate.spawn((message) {
+    print("Message inside isolate: $message");
+  }, user);
+}
+
+class User {
+  final String name;
+  final String email;
+  User({required this.name, required this.email});
+
+  @override
+  String toString() => 'User(name: $name, email: $email)';
 }
