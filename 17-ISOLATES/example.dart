38c9c79a6123999b7bc450f0dc057982717935fc
bastndev <gohit24@gmail.com>
2023-12-29 11:10:19 -0500

    :arrow_up: Update `new`

--- 17-ISOLATES/example.dart
+++ 17-ISOLATES/example.dart
@@ -1,22 +1,31 @@
+import 'dart:developer';
 import 'dart:isolate';
 
-void main() {
+void main() async {
   final receivePort = ReceivePort();
 
   const iterations = 100000;
 
   final message = [receivePort.sendPort, iterations];
+  String isolate2ID = '';
 
   receivePort.listen((message) {
     print('Message received from isolate 2: $message');
+    String mainIsolateID =
+        Service.getIsolateId(Isolate.current) ?? "Main isolate don't have ID";
+    print('Isolate 1: $mainIsolateID');
+    print('Isolate 2: $isolate2ID');
   });
 
-  Isolate.spawn((message) {
+  final isolate2 = await Isolate.spawn((message) {
     print('Started isolate 2');
     SendPort sendPort = message.first as SendPort;
     int iterations = message.last as int;
-    _createLoop(iterations, 'Isolate 2');
+    int total = _createLoop(iterations, 'Isolate 2');
+    sendPort.send(total);
   }, message);
+
+  isolate2ID = Service.getIsolateId(isolate2) ?? "They don't have isolate 2 ID";
 
   _createLoop(iterations, 'Main isolate');
 }
