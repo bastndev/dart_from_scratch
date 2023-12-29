e957e10c5b2346dc996a516173b647084f9d2d62
bastndev <gohit24@gmail.com>
2023-12-29 10:08:50 -0500

    :arrow_up: Update `new`

--- 17-ISOLATES/create_isolates.dart
+++ 17-ISOLATES/create_isolates.dart
@@ -1,14 +1,28 @@
 import 'dart:developer';
 import 'dart:isolate';
 
-void main() {
+/* void main() {
   final mainIsolate = Isolate.current;
   final mainIsolateID = Service.getIsolateId(mainIsolate);
   print(mainIsolateID);
 
   final number = 5;
 
+
   Isolate.spawn((message) {
     print("Message inside isolate: $message");
   }, number);
+}
+ */
+
+void main() {
+  final mainIsolate = Isolate.current;
+  final mainIsolateID = Service.getIsolateId(mainIsolate);
+  print(mainIsolateID);
+
+  final list = [1, 2, 3, 4, 5];
+
+  Isolate.spawn((message) {
+    print("Message inside isolate: $message");
+  }, list);
 }
