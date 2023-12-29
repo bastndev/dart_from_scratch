cab9f8172479da695f3dc2dd6e1489fdf9033655
bastndev <gohit24@gmail.com>
2023-12-29 14:43:26 -0500

    :arrow_up: Update `new`

--- 18-CLI/sum.dart
+++ 18-CLI/sum.dart
@@ -1,4 +1,22 @@
+import 'package:args/args.dart';
+
 void main(List<String> args) {
+  final addAll = 'addAll';
+  final argParse = ArgParser()..addFlag(addAll, abbr: 'a');
+  final argResults = argParse.parse(args);
+  final arguments = argResults.rest;
+  bool shouldAddAll = argResults[addAll] as bool;
+  if (shouldAddAll) {
+    final total = arguments.fold(0.0, (previousValue, element) {
+      final value = num.tryParse(element);
+
+      return previousValue + (value ?? 0);
+    });
+    print(total);
+
+    return;
+  }
+
   try {
     final a = num.parse(args.first);
     final b = num.parse(args[1]);
