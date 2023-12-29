351f87abf334285c154afeb830a9f9673e928c62
bastndev <gohit24@gmail.com>
2023-12-29 10:29:43 -0500

    :arrow_up: Update `new`

--- 17-ISOLATES/message_passing.dart
+++ 17-ISOLATES/message_passing.dart
@@ -10,8 +10,10 @@ void main() {
   final user = User(name: 'Gohit', email: 'test@gmail.com');
   final message = [receivePor.sendPort, user];
 
-  Isolate.spawn((message) {
-    
+  Isolate.spawn((message) async {
+    final sendPort = message.first as SendPort;
+    await Future.delayed(const Duration(seconds: 3));
+    sendPort.send("Sending the isolate to the main user.");
   }, message);
 }
 
