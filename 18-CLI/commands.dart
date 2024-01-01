dcb94672eca0f04491f66a7c00899ea0015895e0
bastndev <gohit24@gmail.com>
2024-01-01 10:47:45 -0500

    :arrow_up: Update `recent`

--- 18-CLI/commands.dart
+++ 18-CLI/commands.dart
@@ -1,9 +1,15 @@
 import 'dart:io';
 
-void main() async {
+/* void main() async {
   final dir = await Process.start('dir', [], runInShell: true);
 
   dir.stdout.listen((event) {
     print(String.fromCharCodes(event));
   });
+}
+ */
+
+void main() async {
+  final dir = await Process.run('dir', [], runInShell: true);
+  print(dir.stdout);
 }
