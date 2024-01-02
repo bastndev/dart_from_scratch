b0dac7a0774807328029953b5f0103ef1b7332b7
bastndev <gohit24@gmail.com>
2024-01-01 22:28:05 -0500

    :arrow_up: Update `RECENT`

--- 18-CLI/scraper/bin/database/data_base.dart
+++ 18-CLI/scraper/bin/database/data_base.dart
@@ -1,1 +1,5 @@
-//Create a correct ortographic pls thanks
+//Create a correct orthographic pls thanks
+//dao 
+//models 
+
+// We have arranged the configuration file so that it can be read from any configuration path and use docker to save data
