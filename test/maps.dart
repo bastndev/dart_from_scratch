a0690ad3ca368ced74ba746e9f850d639ac05f8b
bastndev <gohit24@gmail.com>
2024-01-02 18:53:21 -0500

    :arrow_up: Update `RECENT`

---
+++ test/maps.dart
@@ -0,0 +1,15 @@
+import 'package:test/test.dart';
+import 'package:uuid/uuid.dart';
+
+void main() {
+  final map = <String, dynamic>{
+    'id': Uuid().v4(),
+    'name': 'Dart Testing',
+    'email': 'dart@test.com',
+    'object': 'Map'
+  };
+
+
+
+    
+}
