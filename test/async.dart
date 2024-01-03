e9775694c20e1f50fba61c9a8b09962270beeda8
bastndev <gohit24@gmail.com>
2024-01-02 21:32:24 -0500

    :arrow_up: Update `RECENT`

--- test/async.dart
+++ test/async.dart
@@ -1,6 +1,24 @@
-import 'package:test/test.dart';
+import 'dart:convert';
 
-// GOALS 
+import 'package:test/test.dart';
+import 'package:http/http.dart' as http;
+
+// GOALS
 //1.test asynchronous function
 //2.Use mocks
 
+class ReqResAPI {
+  const ReqResAPI();
+
+  Future<Map<String, String>> getAll(http.Client client) async {
+    try {
+      final response =
+          await client.get(Uri.parse('https://reqres.in/api/users'));
+      return jsonDecode(response.body);
+    } catch (_) {
+      rethrow;
+    }
+  }
+}
+
+void main() {}
