9a093659d25c9aabea010e8b3a101e8a7cdb5f9a
bastndev <gohit24@gmail.com>
2024-01-02 19:29:38 -0500

    :arrow_up: Update `RECENT`

--- test/calculator.dart
+++ test/calculator.dart
@@ -1,9 +1,12 @@
 import 'package:test/test.dart';
 
 class Calculator {
-
+  num result = 0.0;
+  Calculator sum(List<num> numbers) {
+    result = numbers.fold(
+        result, (previousValue, element) => previousValue + element);
+    return this;
+  }
 }
 
-void main() { 
-  
-}
+void main() {}
