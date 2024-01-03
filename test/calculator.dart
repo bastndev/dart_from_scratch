f90e446ebfd7d60c83aa59c79de8a5728ea12cad
bastndev <gohit24@gmail.com>
2024-01-02 19:33:40 -0500

    :arrow_up: Update `RECENT`

--- test/calculator.dart
+++ test/calculator.dart
@@ -7,6 +7,28 @@ class Calculator {
         result, (previousValue, element) => previousValue + element);
     return this;
   }
+
+  Calculator subtract(List<num> numbers) {
+    result = numbers.fold(
+        result, (previousValue, element) => previousValue - element);
+    return this;
+  }
+
+  Calculator multiply(List<num> numbers) {
+    result = numbers.fold(
+        result, (previousValue, element) => previousValue * element);
+    return this;
+  }
+
+  Calculator divide(List<num> numbers) {
+    if (numbers.any((element) => element == 0)) {
+      throw Exception("don't know how to divide");
+    }
+    result = numbers.fold(
+        result, (previousValue, element) => previousValue / element);
+    return this;
+  }
+  num get res => result;
 }
 
 void main() {}
